91c89ad2a0cae3c9bcdc9824d078d42848af2e8b
HaeMin Yoon <49224104+Yoon-Hae-Min@users.noreply.github.com>
2022-06-25 22:52:22 +0900

    chore: 프로젝트 기본 세팅 (#11)
    * chore: 프로젝트 기본 세팅
    
    * chore: eslint 플러그인 추가 및 중복 파일 삭제

---
+++ breaking-front/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+  ignorePatterns: ['*.config.js', 'node_modules'],
+  overrides: [
+    {
+      files: ['**/*.stories.*'],
+      rules: {
+        'import/no-anonymous-default-export': 'off',
+      },
+    },
+  ],
+};
