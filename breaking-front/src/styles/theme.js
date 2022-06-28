88825aef3bd035a55261efb9242a781eac70d1cf
kangju2000 <kangju2000@naver.com>
2022-06-28 15:27:53 +0900

    chore: 전역 theme 설정

---
+++ breaking-front/src/styles/theme.js
@@ -0,0 +1,13 @@
+import PALETTE from 'constants/palette'
+
+const theme = {
+  primary: PALETTE.BLUE,
+  red: PALETTE.RED,
+  yellow: PALETTE.YELLOW,
+  green: PALETTE.GREEN,
+  blue: PALETTE.BLUE,
+  gray: PALETTE.GRAY,
+  white: PALETTE.WHITE,
+};
+
+export default theme;
