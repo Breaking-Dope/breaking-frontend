03754ff6c250e513bf81e50fcec019de4e8a2816
kangju2000 <kangju2000@naver.com>
2022-06-28 15:28:18 +0900

    chore: Storybook에 global style 적용되도록 설정

--- breaking-front/.storybook/preview.js
+++ breaking-front/.storybook/preview.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../src/styles/GlobalStyle';
+import theme from '../src/styles/theme';
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
