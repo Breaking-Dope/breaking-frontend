437dee1ad182b730b72b1409773e172f814ca931
Yoon-Hae-Min <yunhatmi@naver.com>
2022-06-29 17:15:27 +0900

    chore: react-query 초기 설정

--- breaking-front/src/App.js
+++ breaking-front/src/App.js
@@ -1,22 +1,28 @@
-import { GlobalStyle } from 'styles/GlobalStyle';
+import GlobalStyle from 'styles/GlobalStyle';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SocialLogin from 'Pages/SocialLogin/SocialLogin';
 import KakaoRedirect from 'Pages/SocialLogin/KakaoRedirect';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<SocialLogin />}></Route>
-          <Route path="login/kakao" element={<KakaoRedirect />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path="login" element={<SocialLogin />}></Route>
+            <Route path="login/kakao" element={<KakaoRedirect />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
