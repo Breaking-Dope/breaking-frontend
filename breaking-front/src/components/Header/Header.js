63f5a65bade1b9f0120e0af00ab5e756bc836059
kangju2000 <kangju2000@naver.com>
2022-06-29 15:33:26 +0900

    feat: Header의 Form 기능 추가

--- breaking-front/src/components/Header/Header.js
+++ breaking-front/src/components/Header/Header.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import * as Style from './Header.styles';
 import { ReactComponent as LogoIcon } from 'assets/svg/small-logo.svg';
 import { ReactComponent as SearchIcon } from 'assets/svg/search.svg';
 import { ReactComponent as DownArrowIcon } from 'assets/svg/down-arrow.svg';
 
-export function Header({ isLogin, ...props }) {
+export default function Header({ isLogin, ...props }) {
+  const [searchText, setSearchText] = useState('');
+
+  const onChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <Style.HeaderContainer isLogin={isLogin} {...props}>
       <Style.HeaderContent>
@@ -13,8 +23,12 @@ export function Header({ isLogin, ...props }) {
           <Style.LogoContainer>
             <LogoIcon width="100%" height="100%" />
           </Style.LogoContainer>
-          <Style.Form>
-            <Style.SearchInput icon={<SearchIcon />} />
+          <Style.Form onSubmit={handleSubmit}>
+            <Style.SearchInput
+              icon={<SearchIcon />}
+              onChange={onChange}
+              value={searchText}
+            />
           </Style.Form>
         </Style.SearchContent>
         <Style.UserContent>
@@ -37,5 +51,3 @@ export function Header({ isLogin, ...props }) {
 Header.propTypes = {
   isLogin: PropTypes.bool,
 };
-
-Header.defaultProps = {};
