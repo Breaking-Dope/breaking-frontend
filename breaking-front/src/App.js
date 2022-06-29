d8577323e826816db50b82c04a547ca30b4e22c7
Yoon-Hae-Min <yunhatmi@naver.com>
2022-06-29 16:15:47 +0900

    feat: kakao 로그인을 위한 라우터 설정

--- breaking-front/src/App.js
+++ breaking-front/src/App.js
@@ -2,11 +2,21 @@ import { GlobalStyle } from 'styles/GlobalStyle';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
+import { BrouserRoute, Routes, Route } from 'react-router-dom';
+import SocialLogin from 'Pages/SocialLogin/SocialLogin';
+import KakaoRedirect from 'Pages/SocialLogin/KakaoRedirect';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <BrouserRoute>
+        <Routes>
+          <Route path="/login" element={SocialLogin}>
+            <Route path="/kakao" element={KakaoRedirect}></Route>
+          </Route>
+        </Routes>
+      </BrouserRoute>
     </ThemeProvider>
   );
 }
