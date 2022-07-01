788f878f0f0f419c01666c69aee1a913106da90f
HaeMin Yoon <49224104+Yoon-Hae-Min@users.noreply.github.com>
2022-07-01 10:16:12 +0900

    feat: 카카오 로그인 구현 (#23)
    * feat: kakao 로그인을 위한 라우터 설정
    
    * feat: 카카오 로그인 인가코드 받기
    
    * chore: react-query 초기 설정
    
    * chore: axios library 추가
    
    * feat: PATH 파일 분리 및 적용
    
    * chore: env파일 gitignore 추가
    
    * feat: redirect 페이지에서 인가코드 받는 기능 추가
    
    * fix: kakao 초기화 함수에 window 객체 제거
    
    * fix: Pages 폴더 pages로 변경

--- breaking-front/src/App.js
+++ breaking-front/src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/GlobalStyle';
 import theme from 'styles/theme';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SocialLogin from 'pages/SocialLogin/SocialLogin';
+import KakaoRedirect from 'pages/SocialLogin/KakaoRedirect';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { PATH } from 'constants/path';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path={PATH.LOGIN} element={<SocialLogin />}></Route>
+            <Route path={PATH.KAKAO_LOGIN} element={<KakaoRedirect />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
