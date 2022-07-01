6fa39ee1ccd70d414630ab8205e6e59442429648
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-01 12:59:06 +0900

    feat: 구글 로그인 기능 추가

--- breaking-front/src/App.js
+++ breaking-front/src/App.js
@@ -8,22 +8,28 @@ import KakaoRedirect from 'pages/SocialLogin/KakaoRedirect';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { PATH } from 'constants/path';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <BrowserRouter>
-          <Routes>
-            <Route path={PATH.LOGIN} element={<SocialLogin />}></Route>
-            <Route path={PATH.KAKAO_LOGIN} element={<KakaoRedirect />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_KEY}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <BrowserRouter>
+            <Routes>
+              <Route path={PATH.LOGIN} element={<SocialLogin />}></Route>
+              <Route
+                path={PATH.KAKAO_LOGIN}
+                element={<KakaoRedirect />}
+              ></Route>
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
   );
 }
 
--- breaking-front/src/pages/SocialLogin/SocialLogin.js
+++ breaking-front/src/pages/SocialLogin/SocialLogin.js
@@ -1,5 +1,6 @@
 import { KAKAO_PATH } from 'constants/path';
 import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
 
 const SocialLogin = () => {
   const { Kakao } = window;
@@ -11,10 +12,21 @@ const SocialLogin = () => {
       redirectUri: KAKAO_PATH.REDIRECT_URL,
     });
   };
+  const googleLoginSuccess = (credentialResponse) => {
+    console.log(credentialResponse);
+    // 백엔드에 토큰을 던져주는 API를 실행
+  };
+  const googleLoginError = () => {
+    //에러 페이지 이동
+  };
+
   return (
     <>
       <div onClick={KakaoLoginOnClick}>카카오 로그인</div>
-      <div>구글 로그인</div>
+      <GoogleLogin
+        onSuccess={googleLoginSuccess}
+        onError={googleLoginError}
+      ></GoogleLogin>
     </>
   );
 };
