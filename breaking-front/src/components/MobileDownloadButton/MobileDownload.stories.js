81bffed0376697abd09bc043a8c11a08e5e70380
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-02 00:10:05 +0900

    feat: 모바일 다운로드 아이콘 버튼 UI 구현

---
+++ breaking-front/src/components/MobileDownloadButton/MobileDownload.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import MobileDownloadButton from './MobileDownloadButton';
+
+export default {
+  title: 'components/MobileDownloadButton',
+  component: MobileDownloadButton,
+  argTypes: {
+    social: {
+      options: ['appstore', 'playstore'],
+      control: { type: 'radio' },
+    },
+  },
+};
+
+function Template(args) {
+  return <MobileDownloadButton {...args} />;
+}
+
+export const AppStore = Template.bind({});
+AppStore.args = {
+  social: 'appstore',
+};
+
+export const PlayStore = Template.bind({});
+PlayStore.args = {
+  social: 'playstore',
+};
---
+++ breaking-front/src/components/MobileDownloadButton/MobileDownload.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const MobileDownloadButton = styled.button`
+  padding: 10px;
+  border: none;
+  background-color: white;
+  cursor: pointer;
+`;
