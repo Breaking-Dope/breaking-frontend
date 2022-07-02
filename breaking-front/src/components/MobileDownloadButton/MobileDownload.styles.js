71d34794901e28e69aee4714b234a9ffc901a3db
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-02 21:24:00 +0900

    refactor: 색상값을 전역 색상으로 변경

--- breaking-front/src/components/MobileDownloadButton/MobileDownload.styles.js
+++ breaking-front/src/components/MobileDownloadButton/MobileDownload.styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const MobileDownloadButton = styled.button`
   padding: 10px;
   border: none;
-  background-color: white;
+  background-color: ${({ theme }) => theme.white};
   cursor: pointer;
 `;
--- breaking-front/src/components/SocialLoginButton/SocialLoginButton.styles.js
+++ breaking-front/src/components/SocialLoginButton/SocialLoginButton.styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const LoginButton = styled.button`
   padding: 10px;
   border: none;
-  background-color: white;
+  background-color: ${({ theme }) => theme.white};
   cursor: pointer;
 `;
