b70673305fa51df8e00217498f646d86cda8d688
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-03 20:24:14 +0900

    style: 화면의 크기 꽉차게 설정

--- breaking-front/src/styles/GlobalStyle.js
+++ breaking-front/src/styles/GlobalStyle.js
@@ -4,6 +4,21 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
   ${reset};
 
+
+  html{
+    height:100%;
+  }
+
+  body{
+    min-height:100%;
+  }
+
+  #root{
+    width:100%;
+    position:absolute;
+    min-height:100%;
+  }
+
   * {
     box-sizing: border-box;
   }
