e811d97a592a01502fc3fff6b675b498df068f58
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-04 13:15:25 +0900

    fix: SocialLoginButton 수정

--- breaking-front/src/components/SocialLoginButton/SocialLoginButton.styles.js
+++ breaking-front/src/components/SocialLoginButton/SocialLoginButton.styles.js
@@ -1,8 +1,18 @@
 import styled from 'styled-components';
+import KakaoLoginIcon from 'assets/svg/kakao_login.svg';
+import GoogleLoginIcon from 'assets/svg/google_login.svg';
+
+const socialIcon = {
+  kakao: KakaoLoginIcon,
+  google: GoogleLoginIcon,
+};
 
 export const LoginButton = styled.button`
+  width: 252px;
+  height: 60px;
   padding: 10px;
   border: none;
+  background-image: url(${({ icon }) => socialIcon[icon]});
   background-color: ${({ theme }) => theme.white};
   cursor: pointer;
 `;
