b65e9aa44f3ee9e273d906ab0c4bc518f1bd05df
KangJH(주기) <kangju2000@naver.com>
2022-07-04 14:09:56 +0900

    feat: SignUpInput 컴포넌트 구현 (#45)
    * feat: SignUpInput 컴포넌트 구현
    
    * refactor: 불필요한 속성 제거

---
+++ breaking-front/src/components/SignUpInput/SignUpInput.styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  display: block;
+  position: relative;
+  padding-bottom: 40px;
+`;
+
+export const LabelText = styled.span`
+  font-size: 16px;
+`;
+
+export const SignUpInput = styled.input`
+  width: 100%;
+  height: 56px;
+  margin-top: 15px;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.gray[200]};
+  font-size: 12px;
+  outline: none;
+`;
+
+export const Message = styled.p`
+  position: absolute;
+  margin: 5px;
+  color: ${({ theme }) => theme.red[900]};
+  font-size: 12px;
+`;
