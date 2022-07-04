a560a9ab62aa4477d00c9f6791ec4a3b56b59146
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-04 15:24:39 +0900

    style: 페이지 높이를 vh 변경에 따른 globalStyle 값 삭제

--- breaking-front/src/styles/GlobalStyle.js
+++ breaking-front/src/styles/GlobalStyle.js
@@ -4,12 +4,6 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
   ${reset};
 
-
-  html, body, #root{
-  height: 100%;
-  min-height:100%;
-}
-
   * {
     box-sizing: border-box;
   }
