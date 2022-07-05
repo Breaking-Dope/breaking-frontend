5bfd10d9663d83ed94595f810e12d08c2bf26157
HaeMin Yoon <49224104+Yoon-Hae-Min@users.noreply.github.com>
2022-07-05 10:42:45 +0900

    feat: 로그인 페이지 UI 생성 (#48)
    * refactor: 라이브러리에서 제공한 UI에서 SocialLoginButton으로 UI변경
    
    * style: breaking 로고 추가
    
    * style: 화면의 크기 꽉차게 설정
    
    * style: UI 컴포넌트 위치별 로드 및 분류
    
    * style: 페이지 레이아웃 추가
    
    * refactor: global layout추가에 따른 header 제거
    
    * feat: 높이 스크롤바 없이 수정 및 세부 margin 추가
    
    * style: 페이지 높이를 vh 변경에 따른 globalStyle 값 삭제
    
    * refactor: 코딩 컨벤션 수정

--- breaking-front/src/pages/SocialLogin/SocialLogin.js
+++ breaking-front/src/pages/SocialLogin/SocialLogin.js
@@ -1,33 +1,46 @@
 import { KAKAO_PATH } from 'constants/path';
 import React from 'react';
-import { GoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
+import SocialLoginButton from 'components/SocialLoginButton/SocialLoginButton';
+import * as Style from 'pages/SocialLogin/SocialLogin.style';
+import Line from 'components/Line/Line';
+import MobileDownloadButton from 'components/MobileDownloadButton/MobileDownloadButton';
 
 const SocialLogin = () => {
+  const googleLoginClick = useGoogleLogin({
+    onSuccess: (credentialResponse) => {
+      console.log(credentialResponse);
+    },
+    onError: () => {},
+  });
   const { Kakao } = window;
   if (!Kakao.isInitialized()) {
     Kakao.init(process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY);
   }
-  const KakaoLoginOnClick = () => {
+  const kakaoLoginClick = () => {
     Kakao.Auth.authorize({
       redirectUri: KAKAO_PATH.REDIRECT_URL,
     });
   };
-  const googleLoginSuccess = (credentialResponse) => {
-    console.log(credentialResponse);
-    // 백엔드에 토큰을 던져주는 API를 실행
-  };
-  const googleLoginError = () => {
-    //에러 페이지 이동
-  };
 
   return (
-    <>
-      <div onClick={KakaoLoginOnClick}>카카오 로그인</div>
-      <GoogleLogin
-        onSuccess={googleLoginSuccess}
-        onError={googleLoginError}
-      ></GoogleLogin>
-    </>
+    <Style.Body>
+      <Style.BreakingLogo />
+      <Style.LoginContent>
+        <Style.LoginTitle>
+          Breaking을 통해
+          <br />
+          지금 바로 제보하세요
+        </Style.LoginTitle>
+        <SocialLoginButton social="kakao" onClick={kakaoLoginClick} />
+        <SocialLoginButton social="google" onClick={googleLoginClick} />
+        <Line />
+        <div>
+          <MobileDownloadButton social="appstore" />
+          <MobileDownloadButton social="playstore" />
+        </div>
+      </Style.LoginContent>
+    </Style.Body>
   );
 };
 
