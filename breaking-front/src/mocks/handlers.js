b2bd2d4414eb938ad974fa66270532871c01630a
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-05 12:13:58 +0900

    feat: 카카오 구글 로그인 mock api 작성
    - 카카오 로그인은 회원가입으로 이동하게 데이터를 반환함
    - 구글 로그인은 로그인이 완료되게 데이터를 반환함

--- breaking-front/src/mocks/handlers.js
+++ breaking-front/src/mocks/handlers.js
@@ -38,4 +38,30 @@ export const handlers = [
     else if (req.body.email === 'kangju2000') return res(ctx.status(400));
     else return res(ctx.status(200));
   }),
+
+  rest.post(PATH.OAUTH2_SIGNIN_KAKAO, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        fullname: '만두피', //기존 사용자의 닉네임 혹은 이름
+
+        username: '더미1', //각 서비스의 회원 정보 코드 -> 고유한 유저 정보
+
+        email: 'email@email.com', //이메일
+
+        profileImgURL: 'URL',
+      })
+    );
+  }),
+
+  rest.post(PATH.OAUTH2_SIGNIN_GOOGLE, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.set('Authorization', 'Bearer abcdefghijklmnop'),
+      ctx.json({
+        profileImgURL: 'URL',
+        nickname: '만두피',
+      })
+    );
+  }),
 ];
