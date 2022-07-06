e6bb35573d989231e9b089c236a215a127fd8253
kangju2000 <kangju2000@naver.com>
2022-07-07 01:02:44 +0900

    refactor: 프로필 토글 position 수정

--- breaking-front/src/components/ProfileToggle/ProfileToggle.styles.js
+++ breaking-front/src/components/ProfileToggle/ProfileToggle.styles.js
@@ -2,11 +2,24 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const ProfileToggleContainer = styled.div`
+  position: relative;
+  display: inline-block;
+  z-index: 99;
+  margin-top: 20px;
+`;
+
+export const ArrowMark = styled.div`
   position: absolute;
   display: inline-block;
-  right: -33px;
-  z-index: 99;
-  margin-top: 70px;
+  z-index: 1;
+  top: -12px;
+  right: 30px;
+  width: 25px;
+  height: 25px;
+  border: 1px solid ${({ theme }) => theme.black};
+  border-width: 1px 0px 0px 1px;
+  background-color: ${({ theme }) => theme.white};
+  transform: rotate(45deg);
 `;
 
 export const ProfileToggle = styled.div`
@@ -15,20 +28,6 @@ export const ProfileToggle = styled.div`
   padding: 15px 10px;
   border: 1px solid ${({ theme }) => theme.black};
   border-radius: 10px;
-`;
-
-export const ArrowMark = styled.div`
-  position: absolute;
-  display: inline-block;
-  z-index: 1;
-  top: -13px;
-  right: 30px;
-  width: 25px;
-  height: 25px;
-  border: 1px solid ${({ theme }) => theme.black};
-  border-width: 1px 0px 0px 1px;
-  background-color: ${({ theme }) => theme.white};
-  transform: rotate(45deg);
 `;
 
 export const LabelLink = styled(Link)`
