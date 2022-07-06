fb63bcd2784316c0f7924e5a5856c4f1f045fd2a
kangju2000 <kangju2000@naver.com>
2022-07-06 14:37:27 +0900

    refactor: status prop의 기본 값을 defaultProps에 추가하여 다른 컴포넌트와 통일성을 맞춤

--- breaking-front/src/components/Input/Input.js
+++ breaking-front/src/components/Input/Input.js
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import * as Style from 'components/Input/Input.styles';
 
-function Input({ status = 'default', icon, ...props }, ref) {
+function Input({ status, icon, ...props }, ref) {
   return (
     <Style.InputContainer>
       <Style.Input ref={ref} {...props} />
@@ -14,6 +14,10 @@ function Input({ status = 'default', icon, ...props }, ref) {
 Input.propTypes = {
   status: PropTypes.oneOf(['default', 'error']),
   icon: PropTypes.element,
+};
+
+Input.defaultProps = {
+  status: 'default',
 };
 
 export default forwardRef(Input);
