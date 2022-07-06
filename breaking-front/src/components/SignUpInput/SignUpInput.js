bd7e82c79080eec3ace24ae5ff92468e5b2d8f20
KangJH(주기) <kangju2000@naver.com>
2022-07-06 15:15:24 +0900

    fix: displayName 추가, SignUpInput에 forwardRef 추가 (#61)

--- breaking-front/src/components/SignUpInput/SignUpInput.js
+++ breaking-front/src/components/SignUpInput/SignUpInput.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import * as Style from 'components/SignUpInput/SignUpInput.styles';
 
-export default function SignUpInput({ label, errorMessage, ...props }) {
+const SignUpInput = forwardRef(({ label, errorMessage, ...props }, ref) => {
   return (
     <Style.Label>
       <Style.LabelText>{label}</Style.LabelText>
-      <Style.SignUpInput {...props} />
+      <Style.SignUpInput ref={ref} {...props} />
       {errorMessage && <Style.Message>{errorMessage}</Style.Message>}
     </Style.Label>
   );
-}
+});
 
 SignUpInput.propTypes = {
   label: PropTypes.string.isRequired,
   errorMessage: PropTypes.string,
 };
+
+SignUpInput.displayName = 'SignUpInput';
+
+export default SignUpInput;
