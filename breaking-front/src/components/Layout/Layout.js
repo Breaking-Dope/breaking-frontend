f208ec2ba21404615f498e6c0155b22c4210c5a1
kangju2000 <kangju2000@naver.com>
2022-07-07 13:42:43 +0900

    refactor: Layout 컴포넌트에 isLogin prop 추가

--- breaking-front/src/components/Layout/Layout.js
+++ breaking-front/src/components/Layout/Layout.js
@@ -5,17 +5,18 @@ import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 import { PATH } from 'constants/path';
 
-export default function Layout({ children }) {
+export default function Layout({ isLogin, children }) {
   const navigate = useNavigate();
   const moveLoginPage = () => navigate(PATH.LOGIN);
   return (
     <>
-      <Header loginButtonClick={moveLoginPage} />
+      <Header isLogin={isLogin} loginButtonClick={moveLoginPage} />
       <Style.Container>{children}</Style.Container>
     </>
   );
 }
 
 Layout.propTypes = {
+  isLogin: PropTypes.bool,
   children: PropTypes.node.isRequired,
 };
