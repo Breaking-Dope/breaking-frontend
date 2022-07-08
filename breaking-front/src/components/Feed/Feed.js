c89535fc7ff719acc107076bc1fbde1b55365a5a
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-08 12:36:14 +0900

    style: Feed 컴포넌트 구조 변경
    content 부분을 3개의 레이아웃으로 만들고 디테일한 값들은 props로 받아서 처리하는 방법을 채택

--- breaking-front/src/components/Feed/Feed.js
+++ breaking-front/src/components/Feed/Feed.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import * as Style from 'components/Feed/Feed.styles';
 import UserImage from 'components/UserImage/UserImage';
 
 import { ReactComponent as LocationIcon } from 'assets/svg/location.svg';
+import { ReactComponent as LikeIcon } from 'assets/svg/like.svg';
+import { ReactComponent as LikedIcon } from 'assets/svg/liked.svg';
+import { ReactComponent as BookMarkIcon } from 'assets/svg/bookmark.svg';
+import { ReactComponent as BookMarkedIcon } from 'assets/svg/bookmarked.svg';
+import { ReactComponent as ETCIcon } from 'assets/svg/etc.svg';
+
+import Button from 'components/Button/Button';
 
 export function Feed({ children, ...props }) {
   return <Style.Feed {...props}>{children}</Style.Feed>;
@@ -33,22 +40,64 @@ export function ProfileImage({ src, ...props }) {
 
 Feed.ProfileImage = ProfileImage;
 
-export function Location({ children, ...props }) {
+export function Context({ title, time, location, isSold, postType, cost }) {
   return (
-    <Style.Location {...props}>
-      <LocationIcon />
-      {children}
-    </Style.Location>
+    <Style.Context>
+      <Style.Title>{title}</Style.Title>
+      <Style.Detail>
+        <LocationIcon />
+        {location}
+      </Style.Detail>
+      <Style.Detail>{time}</Style.Detail>
+      {postType === 'EXCLUSIVE' && (
+        <Button color="dark" size="small">
+          단독
+        </Button>
+      )}
+      {isSold ? (
+        <Button color="danger" size="small">
+          판매 완료
+        </Button>
+      ) : (
+        <Button color="primary" size="small">
+          판매중
+        </Button>
+      )}
+      <Style.Cost>{cost} 원</Style.Cost>
+    </Style.Context>
   );
 }
+Feed.Context = Context;
 
-Feed.Location = Location;
-
-Feed.Title = Style.Title;
 Feed.Content = Style.Content;
-Feed.Cost = Style.Cost;
-Feed.Time = Style.Location;
-Feed.IconWrapper = Style.IconWrapper;
+
+export function Icons({ likeCount, isLiked, isBookmarked }) {
+  const [liked, setLiked] = useState(isLiked);
+  const [bookmarked, setBookmarked] = useState(isBookmarked);
+  const [count, setCount] = useState(likeCount);
+  const toogleLiked = () => {
+    liked ? setCount((pre) => pre - 1) : setCount((pre) => pre + 1);
+    setLiked((pre) => !pre);
+  };
+  const toogleBookmarked = () => {
+    setBookmarked((pre) => !pre);
+  };
+  return (
+    <Style.Icons>
+      <Style.IconWrapper onClick={toogleLiked}>
+        {liked ? <LikedIcon /> : <LikeIcon />}
+      </Style.IconWrapper>
+      <Style.IconWrapper onClick={toogleLiked}>{count}</Style.IconWrapper>
+      <Style.IconWrapper onClick={toogleBookmarked}>
+        {bookmarked ? <BookMarkedIcon /> : <BookMarkIcon />}
+      </Style.IconWrapper>
+      <Style.IconWrapper>
+        <ETCIcon />
+      </Style.IconWrapper>
+    </Style.Icons>
+  );
+}
+Feed.Icons = Icons;
 
 Feed.propTypes = {
   children: PropTypes.object,
@@ -62,6 +111,17 @@ ProfileImage.propTypes = {
   src: PropTypes.string,
 };
 
-Location.propTypes = {
-  children: PropTypes.string,
+Context.propTypes = {
+  title: PropTypes.string,
+  time: PropTypes.string, // 타입 확실하지 않음
+  location: PropTypes.string,
+  isSold: PropTypes.bool,
+  postType: PropTypes.string,
+  cost: PropTypes.number,
+};
+
+Icons.propTypes = {
+  likeCount: PropTypes.number,
+  isLiked: PropTypes.bool,
+  isBookmarked: PropTypes.bool,
 };
--- breaking-front/src/components/Feed/Feed.styles.js
+++ breaking-front/src/components/Feed/Feed.styles.js
@@ -6,17 +6,12 @@ export const Feed = styled.div`
   height: 404px;
   background-color: ${({ theme }) => theme.gray[50]};
   filter: drop-shadow(2px 2px 2px ${({ theme }) => theme.gray[300]});
-  cursor: pointer;
-`;
-
-export const Title = styled.h1`
-  font-size: 18px;
-  font-weight: bold;
 `;
 
 export const FeedImage = styled.img`
   width: 100%;
   height: 300px;
+  cursor: pointer;
 `;
 
 export const FeedDefaultImage = styled.img.attrs({
@@ -24,10 +19,36 @@ export const FeedDefaultImage = styled.img.attrs({
 })`
   width: 100%;
   height: 300px;
+  cursor: pointer;
   background-color: ${({ theme }) => theme.gray[100]};
 `;
 
-export const Location = styled.div`
+export const Content = styled.div`
+  display: flex;
+  width: 400px;
+  height: 100px;
+  padding: 10px;
+  align-items: center;
+`;
+export const ProfileWrapper = styled.div`
+  cursor: pointer;
+`;
+export const Context = styled.div`
+  flex-direction: column;
+  margin-left: 12px;
+  font-size: 12px;
+  > * {
+    margin: 4px;
+  }
+`;
+
+export const Title = styled.a`
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+export const Detail = styled.div`
   font-size: 12px;
   color: ${({ theme }) => theme.gray[800]};
 `;
@@ -39,21 +60,18 @@ export const Cost = styled.p`
   color: ${({ theme }) => theme.blue[900]};
 `;
 
-export const Content = styled.div`
-  display: flex;
-  width: 400px;
-  height: 100px;
-  padding: 10px;
-  align-items: center;
-`;
-
 export const IconWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  > svg {
-    margin-right: 7px;
-  }
+  margin-right: 8px;
+  cursor: pointer;
 `;
 
-export const ProfileWrapper = styled.div``;
+export const Icons = styled.div`
+  display: flex;
+  position: absolute;
+  right: 0;
+  align-items: center;
+  margin-right: 5px;
+`;
