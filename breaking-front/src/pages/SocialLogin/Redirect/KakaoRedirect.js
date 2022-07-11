9ef9ce9742f9ac38274802835031c01a0fef4f90
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-11 15:32:43 +0900

    feat: google 로그인 방식을 redirect방식으로 변경
    1. redirect 변경에 따른 폴더 구조변경
    2. 토큰을 받아오는 postAccessCode 하나로 통일

--- breaking-front/src/pages/SocialLogin/Redirect/KakaoRedirect.js
+++ breaking-front/src/pages/SocialLogin/Redirect/KakaoRedirect.js
@@ -1,4 +1,4 @@
-import { postAccessCode } from 'api/login';
+import { postAccessCode, postSignInWithKakao } from 'api/login';
 import { KAKAO_PATH, PATH } from 'constants/path';
 import React, { useEffect } from 'react';
 import { useMutation } from 'react-query';
@@ -6,35 +6,47 @@ import { useNavigate } from 'react-router-dom';
 
 const KakaoRedirect = () => {
   const navigate = useNavigate();
+
   const accessCode = new URL(window.location.href).searchParams.get('code');
-  const getKakaoToken = useMutation(postAccessCode, {
+
+  const SignUpKakao = useMutation(postSignInWithKakao, {
     onSuccess: (res) => {
       console.log(res);
       const jwtToken = res.headers.authorization;
       if (jwtToken) {
-        localStorage.setItem({
-          key: 'access_token',
-          value: res.headers.authorization,
-        });
+        localStorage.setItem('access_token', res.headers.authorization);
         navigate(PATH.HOME);
       } else {
         navigate(PATH.SIGNUP, { state: res.data });
       }
     },
+  });
+
+  const GetKakaoToken = useMutation(postAccessCode, {
+    onSuccess: (res) => {
+      console.log(res);
+      SignUpKakao.mutate({
+        accessToken: res.data.access_token,
+      });
+    },
     onError: () => {
       //에러 페이지 이동
     },
   });
+
   useEffect(() => {
-    getKakaoToken.mutate({
-      grant_type: 'authorization_code',
-      client_id: process.env.REACT_APP_REST_API_KEY,
-      redirect_uri: KAKAO_PATH.REDIRECT_URL,
-      code: accessCode,
+    GetKakaoToken.mutate({
+      url: KAKAO_PATH.OAUTH_TOKEN,
+      data: {
+        grant_type: 'authorization_code',
+        client_id: process.env.REACT_APP_REST_API_KEY,
+        redirect_uri: KAKAO_PATH.REDIRECT_URL,
+        code: accessCode,
+      },
     });
   }, []);
 
-  return <div />;
+  return <></>;
 };
 
 export default KakaoRedirect;
