4c99d1556270c40731aa7b09291370f74718b153
kangju2000 <kangju2000@naver.com>
2022-07-11 12:11:22 +0900

    refactor: storybook-addon-state 삭제

--- breaking-front/src/components/Tabs/Tabs.stories.js
+++ breaking-front/src/components/Tabs/Tabs.stories.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tabs from 'components/Tabs/Tabs';
-import useState from 'storybook-addon-state';
 
 export default {
   title: 'components/Tabs',
