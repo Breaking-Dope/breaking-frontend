3febadaca539640d808e4d492a72b3153774827e
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-12 11:31:39 +0900

    feat: state 관리를 컴포넌트안에서 하도록 변경

--- breaking-front/src/components/Filter/Filter.js
+++ breaking-front/src/components/Filter/Filter.js
@@ -1,46 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ReactComponent as FilterIcon } from 'assets/svg/filter.svg';
 import * as Style from 'components/Filter/Filter.styles';
 
-export default function Filter({
-  width,
-  selectedFilter,
-  isOpen,
-  children,
-  ...props
-}) {
+export default function Filter({ width, initalize, children, ...props }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedFilter, setSelectedFilter] = useState(initalize);
+  const prop = {
+    filterClick: (event) => {
+      setSelectedFilter(event.target.innerText);
+      setIsOpen(false);
+    },
+  };
+
+  // React.cloneElement(children.map(()=>{
+
+  // }))
   return (
     <Style.FilterContainer width={width} {...props}>
-      <Style.IconContainer>
+      <Style.IconContainer onClick={() => setIsOpen((pre) => !pre)}>
         <FilterIcon />
       </Style.IconContainer>
       <Style.Filter isOpen={isOpen}>
-        {isOpen ? <>{children}</> : <Style.Label>{selectedFilter}</Style.Label>}
+        {isOpen ? (
+          <>
+            {children.map((child) => {
+              return React.cloneElement(child, prop);
+            })}
+          </>
+        ) : (
+          <Style.Label onClick={() => setIsOpen((pre) => !pre)}>
+            {selectedFilter}
+          </Style.Label>
+        )}
       </Style.Filter>
     </Style.FilterContainer>
   );
 }
 
-function FilterDetail({ label, filterClick }) {
-  return <Style.Label onClick={filterClick}>{label}</Style.Label>;
+function FilterDetail({ children, filterClick }) {
+  return <Style.Label onClick={filterClick}>{children}</Style.Label>;
 }
 
 Filter.FilterDetail = FilterDetail;
 
 Filter.propTypes = {
   width: PropTypes.string,
-  selectedFilter: PropTypes.string,
-  isOpen: PropTypes.bool,
+  initalize: PropTypes.string,
   children: PropTypes.node,
 };
 
 Filter.defaultProps = {
   width: '100px',
-  isOpen: false,
 };
 
 FilterDetail.propTypes = {
-  label: PropTypes.string,
+  children: PropTypes.string,
   filterClick: PropTypes.func,
 };
--- breaking-front/src/components/Filter/Filter.stories.js
+++ breaking-front/src/components/Filter/Filter.stories.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Filter from 'components/Filter/Filter';
 
 export default {
@@ -7,32 +7,11 @@ export default {
 };
 
 export const DefaultFilter = () => {
-  const label = [
-    { label: '최신순', id: '1' },
-    { label: '조회순', id: '2' },
-    { label: '좋아요순', id: '3' },
-  ];
-  const [isOpen, setIsOpen] = useState(false);
-  const [index, setIndex] = useState(0);
-
   return (
-    <Filter
-      selectedFilter={label[index].label}
-      onClick={() => setIsOpen((prev) => !prev)}
-      isOpen={isOpen}
-    >
-      <Filter.FilterDetail
-        label={label[0].label}
-        filterClick={() => setIndex(0)}
-      />
-      <Filter.FilterDetail
-        label={label[1].label}
-        filterClick={() => setIndex(1)}
-      />
-      <Filter.FilterDetail
-        label={label[2].label}
-        filterClick={() => setIndex(2)}
-      />
+    <Filter initalize="최신순">
+      <Filter.FilterDetail>최신순</Filter.FilterDetail>
+      <Filter.FilterDetail>좋아요</Filter.FilterDetail>
+      <Filter.FilterDetail>조회수</Filter.FilterDetail>
     </Filter>
   );
 };
