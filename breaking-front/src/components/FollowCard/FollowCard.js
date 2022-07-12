a74e330508371f4920af65225293c82af6c02a92
KangJH(주기) <kangju2000@naver.com>
2022-07-12 14:45:59 +0900

    feat: FollowCard 컴포넌트 구현 (#79)
    * feat: FollowCard 컴포넌트 구현
    
    * feat: palette에 blue 50 색상 추가
    
    * feat: FollowCard에서 프로필 이미지 또는 닉네임 클릭이벤트 prop 추가
    
    * refactor: ProfileImage 명칭 변경에 맞게 FollowCard 컴포넌트에 적용
    
    * fix: 닉네임 태그를 p에서 span으로 변경
    
    * refactor: FollowCard style 속성 선언 순서 맞게 수정
    
    * refactor: prop 이름 수정
    isMyPage -> isPermission

---
+++ breaking-front/src/components/FollowCard/FollowCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as Style from 'components/FollowCard/FollowCard.styles';
+import ProfileImage from 'components/ProfileImage/ProfileImage';
+
+export default function FollowCard({
+  profileData,
+  isPermission,
+  cardClick,
+  deleteClick,
+}) {
+  return (
+    <Style.FollowCard>
+      <ProfileImage
+        src={profileData.profileImgURL}
+        size="medium"
+        onClick={cardClick}
+      />
+      <Style.Container>
+        <Style.Nickname onClick={cardClick}>
+          {profileData.nickname}
+        </Style.Nickname>
+        <Style.StatusMessage>{profileData.statusMsg}</Style.StatusMessage>
+      </Style.Container>
+      {isPermission && (
+        <Style.DeleteButton size="small" onClick={deleteClick}>
+          삭제
+        </Style.DeleteButton>
+      )}
+    </Style.FollowCard>
+  );
+}
+
+FollowCard.propTypes = {
+  profileData: PropTypes.object,
+  isPermission: PropTypes.bool,
+  cardClick: PropTypes.func,
+  deleteClick: PropTypes.func,
+};
---
+++ breaking-front/src/components/FollowCard/FollowCard.styles.js
@@ -0,0 +1,33 @@
+import Button from 'components/Button/Button';
+import styled from 'styled-components';
+
+export const FollowCard = styled.div`
+  display: flex;
+  align-items: center;
+  width: 400px;
+  height: 80px;
+  padding: 15px;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.blue[50]};
+`;
+
+export const Container = styled.div`
+  width: 250px;
+  margin-left: 20px;
+`;
+
+export const Nickname = styled.span`
+  font-size: 12px;
+  cursor: pointer;
+`;
+export const StatusMessage = styled.p`
+  margin-top: 5px;
+  font-size: 8px;
+  color: ${({ theme }) => theme.gray[700]};
+`;
+
+export const DeleteButton = styled(Button)`
+  padding: 5px 10px;
+  color: ${({ theme }) => theme.black};
+  background-color: ${({ theme }) => theme.white};
+`;
