b7160dab345c6f4bf4eb93e8b34d7502dd1726fe
HaeMin Yoon <49224104+Yoon-Hae-Min@users.noreply.github.com>
2022-07-14 12:06:45 +0900

    feat: tabs 컴포넌트 구현한다. (#88)
    * feat: Tabs 컴포넌트 구현
    
    * feat: story에 default tabs 추가
    
    * refactor: storybook-addon-state 삭제
    
    * feat: tabs component 내부에서 state를 관리하도록 변경
    
    * refacotr: key 추가 및 변수 명시화
    
    Co-authored-by: kangju2000 <kangju2000@naver.com>

---
+++ breaking-front/src/components/Tabs/Tabs.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Tabs from 'components/Tabs/Tabs';
+import Button from 'components/Button/Button';
+
+export default {
+  title: 'components/Tabs',
+  component: Tabs,
+  subComponents: {
+    'Tabs.TabList': Tabs.TabList,
+    'Tabs.TabItem': Tabs.TabItem,
+    'Tabs.TabPanel': Tabs.TabPanel,
+  },
+};
+
+export const DefaultTabs = () => {
+  return (
+    <Tabs>
+      <Tabs.TabList>
+        <Tabs.TabItem>작성한 제보(3)</Tabs.TabItem>
+        <Tabs.TabItem>구매한 제보(10)</Tabs.TabItem>
+        <Tabs.TabItem>북마크한 제보(2)</Tabs.TabItem>
+      </Tabs.TabList>
+      <Tabs.TabPanel>
+        <Button>컴포넌트도 잘들어가요</Button>
+      </Tabs.TabPanel>
+      <Tabs.TabPanel>2번째</Tabs.TabPanel>
+      <Tabs.TabPanel>3번째</Tabs.TabPanel>
+    </Tabs>
+  );
+};
