9b918098336afee2973a8d4d0b4ac265d00e5ddd
kangju2000 <kangju2000@naver.com>
2022-07-15 12:25:33 +0900

    feat: MESSAGE에 SIGNUP 메시지 추가

--- breaking-front/src/constants/message.js
+++ breaking-front/src/constants/message.js
@@ -1,3 +1,11 @@
-const MESSAGE = {};
+const MESSAGE = {
+  SIGNUP: {
+    BLANK: '필수로 입력해야합니다.',
+    SUBMIT_INVALID_NICKNAME: '닉네임을 올바르게 기입해주시기 바랍니다.',
+    SUBMIT_INVALID_PHONENUMBER: '전화번호를 올바르게 기입해주시기 바랍니다.',
+    SUBMIT_INVALID_EMAIL: '이메일을 올바르게 기입해주시기 바랍니다.',
+    WRONG_ACCESS: 'SNS 로그인 후 회원가입이 가능함니다.',
+  },
+};
 
 export default MESSAGE;
