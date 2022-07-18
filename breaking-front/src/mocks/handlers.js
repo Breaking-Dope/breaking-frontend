14a93c2a3894f32f61ff1adab5f13b4e7778d496
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-18 17:43:44 +0900

    feat: 팔로우가 되어있지 않은 데이터 mocking api 추가

--- breaking-front/src/mocks/handlers.js
+++ breaking-front/src/mocks/handlers.js
@@ -316,4 +316,153 @@ export const handlers = [
       ])
     );
   }),
+
+  rest.get(API_PATH.PROFILE_WRITTEN(1, 'all'), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          postId: '1',
+          title: '~~~사건',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          balance: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+        },
+        {
+          postId: '2',
+          title: '~~~사건',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          balance: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+        },
+      ])
+    );
+  }),
+  rest.get(API_PATH.PROFILE_WRITTEN(1, 'sold'), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          postId: '1',
+          title: '팔린글',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          balance: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+        },
+        {
+          postId: '2',
+          title: '~~~팔렸어요',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          balance: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+        },
+      ])
+    );
+  }),
+
+  rest.get(API_PATH.PROFILE_DATA(1), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        userId: 0,
+        profileImgURL: '',
+        nickname: '만두피',
+        email: '',
+        realName: '',
+        role: '',
+        statusMsg: '안녕하세요',
+        followerCount: 30,
+        followingCount: 30,
+        postCount: 5,
+        isFollowing: false,
+      })
+    );
+  }),
+
+  rest.get(API_PATH.PROFILE_FOLLOWERS(1), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          userId: '32',
+          nickname: '주기',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '23',
+          nickname: '자기',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '65',
+          nickname: '저기',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+      ])
+    );
+  }),
+  rest.get(API_PATH.PROFILE_FOLLOWINGS(1), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          userId: '32',
+          nickname: '만두피',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '23',
+          nickname: '천두피',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '65',
+          nickname: '백두피',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+      ])
+    );
+  }),
 ];
