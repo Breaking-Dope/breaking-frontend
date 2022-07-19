35e400371b1a5a54c155eca29cdc05c9a6dfa180
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-19 15:19:15 +0900

    feat: nickname 폰트크기를 32px로 조정

--- breaking-front/src/pages/Profile/Profile.styles.js
+++ breaking-front/src/pages/Profile/Profile.styles.js
@@ -18,7 +18,7 @@ export const Title = styled.div`
   align-items: center;
 `;
 export const NickName = styled.h1`
-  font-size: 36px;
+  font-size: 32px;
   font-weight: bold;
 `;
 export const FollowButton = styled.div`
