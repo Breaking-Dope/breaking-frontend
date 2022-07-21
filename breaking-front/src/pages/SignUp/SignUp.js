546f32eff91411dffa93d5ea905ecba25dd0bad4
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-21 12:38:10 +0900

    refactor: 필요없는 부분 제거

--- breaking-front/src/pages/SignUp/SignUp.js
+++ breaking-front/src/pages/SignUp/SignUp.js
@@ -1,25 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useMutation } from 'react-query';
 import { postSignUp } from 'api/signUp';
 import { PAGE_PATH } from 'constants/path';
 import ProfileSettingForm from 'components/ProfileSettingForm/ProfileSettingForm';
 import useJWTValidate from 'hooks/queries/useJWTValidate';
+import MESSAGE from 'constants/message';
 
 const SignUp = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { refetch: JWTValidateRefetch } = useJWTValidate();
   console.log(JWTValidateRefetch);
-  const userDefaultData = {
-    profileImgURL: '',
-    realName: '',
-    nickname: '',
-    phoneNumber: '',
-    email: '',
-    statusMsg: '',
-    role: 'USER',
-  };
 
   const { mutate } = useMutation(postSignUp, {
     onSuccess: (res) => {
@@ -35,21 +27,17 @@ const SignUp = () => {
     },
   });
 
-  // useEffect(() => {
-  //   //유저가 sns 로그인하지않고 회원가입 페이지로 들어왔을 때 처리
-  //   if (!location.state) {
-  //     alert(MESSAGE.SIGNUP.WRONG_ACCESS);
-  //     navigate(PAGE_PATH.LOGIN);
-  //   }
-  // }, []);
+  useEffect(() => {
+    //유저가 sns 로그인하지않고 회원가입 페이지로 들어왔을 때 처리
+    if (!location.state) {
+      alert(MESSAGE.SIGNUP.WRONG_ACCESS);
+      navigate(PAGE_PATH.LOGIN);
+    }
+  }, []);
 
   return (
     <>
-      <ProfileSettingForm
-        username={location.state?.username}
-        userDefaultData={userDefaultData}
-        mutate={mutate}
-      />
+      <ProfileSettingForm username={location.state?.username} mutate={mutate} />
     </>
   );
 };
