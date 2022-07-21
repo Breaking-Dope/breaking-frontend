0eecc74b5c989450345e91a7674b368c057995c2
HaeMin Yoon <49224104+Yoon-Hae-Min@users.noreply.github.com>
2022-07-21 14:58:20 +0900

    refactor: 기능에 맞게 msw handler 분리 (#118)
    * refactor: 기능에 맞게 msw handler 분리
    
    * refactor: 상대경로 절대경로 수정
    
    * refactor: 오타수정

--- breaking-front/src/mocks/profileHandlers.js
+++ breaking-front/src/mocks/profileHandlers.js
@@ -2,23 +2,394 @@ import { API_PATH } from 'constants/path';
 import { rest } from 'msw';
 
 export const profileHandlers = [
-  rest.get(API_PATH.PROFILE_DETAIL_DATA, (req, res, ctx) => {
+  rest.get(API_PATH.PROFILE_WRITTEN(0, 'all'), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          postId: '1',
+          title: '~~~사건',
+          region: '중구',
+          thumbnailImgURL:
+            'https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179_960_720.jpg',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+        {
+          postId: '2',
+          title: '~~~사건',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+      ])
+    );
+  }),
+
+  rest.get(API_PATH.PROFILE_BOUGHT(0, 'all'), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          postId: '3',
+          title: '제목이다',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+        {
+          postId: '4',
+          title: '~~~다른제목',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+      ])
+    );
+  }),
+
+  rest.get(API_PATH.PROFILE_BOOKMARKED(0, 'all'), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          postId: '6',
+          title: '~~~사건',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+        {
+          postId: '7',
+          title: '~~~사건',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+        {
+          postId: '8',
+          title: '~~~사건',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+        {
+          postId: '9',
+          title: '~~~사건',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+      ])
+    );
+  }),
+
+  rest.get(API_PATH.PROFILE_DATA(0), (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
+        userId: 0,
         profileImgURL: '',
-        realName: '강주혁',
-        nickname: '주기',
-        phoneNumber: '01012345678',
-        email: 'kangju2000@naver.com',
-        statusMsg: '안녕하세요!!',
-        role: 'USER',
+        nickname: '만두피',
+        email: '',
+        realName: '',
+        role: '',
+        statusMsg: '안녕하세요',
+        followerCount: 30,
+        followingCount: 30,
+        postCount: 5,
+        isFollowing: false,
       })
     );
   }),
 
-  rest.put(API_PATH.PROFILE_EDIT, (req, res, ctx) => {
-    console.log(req.body);
+  rest.get(API_PATH.PROFILE_FOLLOWERS(0), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          userId: '32',
+          nickname: '주기',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '23',
+          nickname: '자기',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '65',
+          nickname: '저기',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+      ])
+    );
+  }),
+  rest.get(API_PATH.PROFILE_FOLLOWINGS(0), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          userId: '32',
+          nickname: '만두피',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '23',
+          nickname: '천두피',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '65',
+          nickname: '백두피',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+      ])
+    );
+  }),
+
+  rest.get(API_PATH.PROFILE_WRITTEN(1, 'all'), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          postId: '1',
+          title: '~~~사건',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+        {
+          postId: '2',
+          title: '~~~사건',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+      ])
+    );
+  }),
+  rest.get(API_PATH.PROFILE_WRITTEN(1, 'sold'), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          postId: '1',
+          title: '팔린글',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+        {
+          postId: '2',
+          title: '~~~팔렸어요',
+          region: '중구',
+          thumbnailImgURL: '',
+          likeCount: 999,
+          postType: 'EXCLUSIVE',
+          isSold: false,
+          price: 10000,
+          viewCount: 1000,
+          userId: 123,
+          profileImgURL: '',
+          realName: '가나다',
+          isLiked: false,
+          isBookmarked: false,
+          createdTime: new Date(),
+        },
+      ])
+    );
+  }),
+
+  rest.get(API_PATH.PROFILE_DATA(1), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        userId: 0,
+        profileImgURL: '',
+        nickname: '만두피',
+        email: '',
+        realName: '',
+        role: '',
+        statusMsg: '안녕하세요',
+        followerCount: 30,
+        followingCount: 30,
+        postCount: 5,
+        isFollowing: false,
+      })
+    );
+  }),
+
+  rest.get(API_PATH.PROFILE_FOLLOWERS(1), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          userId: '32',
+          nickname: '주기',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '23',
+          nickname: '자기',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '65',
+          nickname: '저기',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+      ])
+    );
+  }),
+  rest.get(API_PATH.PROFILE_FOLLOWINGS(1), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          userId: '32',
+          nickname: '만두피',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '23',
+          nickname: '천두피',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+        {
+          userId: '65',
+          nickname: '백두피',
+          statusMsg: '안녕하세요',
+          profileImgURL: '',
+        },
+      ])
+    );
+  }),
+
+  rest.post(API_PATH.FOLLOW(1), (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+  rest.post(API_PATH.UNFOLLOW(1), (req, res, ctx) => {
     return res(ctx.status(200));
   }),
 ];
---
+++ breaking-front/src/mocks/signInHandlers.js
@@ -0,0 +1,42 @@
+import { API_PATH } from 'constants/path';
+import { rest } from 'msw';
+
+export const signInHandlers = [
+  rest.get(API_PATH.OAUTH2_VALIDATE_JWT, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        userId: 123454,
+        profileImgURL: '',
+        nickname: '주기',
+        balance: 9999999999,
+      }),
+      ctx.status(200)
+    );
+  }),
+
+  rest.post(API_PATH.OAUTH2_SIGNIN_KAKAO, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        fullname: '만두피', //기존 사용자의 닉네임 혹은 이름
+
+        username: '더미1', //각 서비스의 회원 정보 코드 -> 고유한 유저 정보
+
+        email: 'email@email.com', //이메일
+
+        profileImgURL: 'URL',
+      })
+    );
+  }),
+
+  rest.post(API_PATH.OAUTH2_SIGNIN_GOOGLE, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.set('Authorization', 'Bearer abcdefghijklmnop'),
+      ctx.json({
+        profileImgURL: 'URL',
+        nickname: '만두피',
+      })
+    );
+  }),
+];
