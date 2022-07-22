07336dee448666e553251fd0e3a134406211bac9
kangju2000 <kangju2000@naver.com>
2022-07-22 12:03:11 +0900

    feat: MainFeed mocking api 추가

--- breaking-front/src/mocks/handlers.js
+++ breaking-front/src/mocks/handlers.js
@@ -2,10 +2,12 @@ import { profileHandlers } from 'mocks/profileHandlers';
 import { profileSettingHandlers } from 'mocks/profileSettingHandlers';
 import { signInHandlers } from 'mocks/signInHandlers';
 import { signUpHandlers } from 'mocks/signUpHandlers';
+import { mainFeedHandlers } from 'mocks/mainFeedHandlers';
 
 export const handlers = [
   ...profileSettingHandlers,
   ...profileHandlers,
   ...signInHandlers,
   ...signUpHandlers,
+  ...mainFeedHandlers,
 ];
