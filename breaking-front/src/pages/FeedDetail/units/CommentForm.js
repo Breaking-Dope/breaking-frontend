fb0d6a4bcc947bcf0817d39aa01b98df85c56e63
kangju2000 <kangju2000@naver.com>
2022-07-24 16:07:51 +0900

    feat: FeedDetail의 CommentForm 컴포넌트 구현

---
+++ breaking-front/src/pages/FeedDetail/units/CommentForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import ProfileImage from 'components/ProfileImage/ProfileImage';
+import * as Style from 'pages/FeedDetail/units/CommentForm.styles';
+
+const CommentForm = ({ profileImgURL }) => {
+  const [comment, setComment] = useState('');
+
+  const handleChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setComment('');
+  };
+  return (
+    <>
+      <Style.CommentForm onSubmit={handleSubmit}>
+        <ProfileImage size="medium" src={profileImgURL} />
+        <Style.CommentInput
+          placeholder="댓글 추가"
+          onChange={handleChange}
+          value={comment}
+        />
+        <Style.CommentButton type="submit">등록</Style.CommentButton>
+      </Style.CommentForm>
+    </>
+  );
+};
+CommentForm.propTypes = {
+  profileImgURL: PropTypes.string,
+};
+
+export default CommentForm;
