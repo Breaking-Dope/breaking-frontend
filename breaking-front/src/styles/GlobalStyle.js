0dc7e178a7a237998e95d408075fa33200fa9391
KangJH(주기) <kangju2000@naver.com>
2022-07-25 15:20:00 +0900

    feat: pretendard 폰트 추가 (#133)
    * feat: pretendard 폰트 추가
    
    * refactor: 주석 제거

--- breaking-front/src/styles/GlobalStyle.js
+++ breaking-front/src/styles/GlobalStyle.js
@@ -1,10 +1,30 @@
+import PretendardLight from 'styles/fonts/Pretendard-Light.woff';
+import PretendardMedium from 'styles/fonts/Pretendard-Medium.woff';
+import PretendardBold from 'styles/fonts/Pretendard-SemiBold.woff';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset};
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 300;
+    src: url(${PretendardLight}) format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 500;
+    src: url(${PretendardMedium}) format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 700;
+    src: url(${PretendardBold}) format('woff');
+  }
+
   body{
     overflow-y: scroll;
+    font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
   a {
     text-decoration: none;
@@ -13,6 +33,7 @@ const GlobalStyle = createGlobalStyle`
   
   * {
     box-sizing: border-box;
+    font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
 `;
 
