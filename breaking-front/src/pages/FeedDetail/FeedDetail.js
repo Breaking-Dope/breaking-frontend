f424924eeec95f7545dda28e21f26c6e6375b1db
kangju2000 <kangju2000@naver.com>
2022-07-25 15:34:35 +0900

    refactor: 불필요한 eslint 주석 제거

--- breaking-front/src/pages/FeedDetail/FeedDetail.js
+++ breaking-front/src/pages/FeedDetail/FeedDetail.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useContext, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { UserInformationContext } from 'providers/UserInformationProvider';
--- breaking-front/src/pages/FeedDetail/units/Carousel.js
+++ breaking-front/src/pages/FeedDetail/units/Carousel.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import MediaModal from 'pages/FeedDetail/units/MediaModal';
