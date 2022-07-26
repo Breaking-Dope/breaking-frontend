41fd11aba0b2b7c6080f255e1ac3d87a3da42735
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-26 15:31:14 +0900

    feat: 가로축 custom 스크롤바

--- breaking-front/src/components/ScrollBar/ScrollBar.js
+++ breaking-front/src/components/ScrollBar/ScrollBar.js
@@ -14,3 +14,18 @@ export const ScrollBarY = styled.div`
     background: ${({ theme }) => theme.gray[300]};
   }
 `;
+
+export const ScrollBarX = styled.div`
+  overflow-x: auto;
+  &::-webkit-scrollbar {
+    height: 7px;
+  }
+  &::-webkit-scrollbar-track {
+    background: ${({ theme }) => theme.gray[700]};
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: ${({ theme }) => theme.gray[300]};
+  }
+`;
