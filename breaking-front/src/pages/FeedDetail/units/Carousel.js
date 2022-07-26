ac9917ddfbbae8c5c44158e8440df0a075990c08
kangju2000 <kangju2000@naver.com>
2022-07-26 13:08:29 +0900

    refactor: 이미지 클릭 시 Modal 에서 팝업으로 변경

--- breaking-front/src/pages/FeedDetail/units/Carousel.js
+++ breaking-front/src/pages/FeedDetail/units/Carousel.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import MediaModal from 'pages/FeedDetail/units/MediaModal';
 import * as Style from 'pages/FeedDetail/units/Carousel.styles';
 import { ReactComponent as LeftIcon } from 'assets/svg/carousel-left.svg';
 import { ReactComponent as RightIcon } from 'assets/svg/carousel-right.svg';
@@ -10,9 +9,25 @@ import { ReactComponent as SildeDotFillIcon } from 'assets/svg/slide-dot-fill.sv
 const Carousel = ({ mediaList }) => {
   const mediaCount = mediaList.length;
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [modalSrc, setModalSrc] = useState('');
-  const [isOpenMediaModal, setisOpenMediaModal] = useState(false);
   const slideRef = useRef(null);
+
+  const showPopup = (media) => {
+    let image = new Image();
+    image.src = media;
+    let width = image.width;
+    let height = image.height;
+
+    if (width >= 1000) {
+      height *= 1000 / width;
+      width = 1000;
+    }
+
+    return window.open(
+      media,
+      'Popup',
+      `width=${width}, height=${height}, left=600, top=100, scrollbars=no, resizable=no`
+    );
+  };
 
   const prevSlide = () => {
     currentSlide !== 0 && setCurrentSlide((pre) => pre - 1);
@@ -21,33 +36,22 @@ const Carousel = ({ mediaList }) => {
     currentSlide < mediaCount - 1 && setCurrentSlide((pre) => pre + 1);
   };
 
-  const imageClick = (event) => {
-    setModalSrc(event.target.src);
-    setisOpenMediaModal((pre) => !pre);
-  };
-
-  const closeClick = () => {
-    setisOpenMediaModal(false);
-  };
-
   useEffect(() => {
     slideRef.current.style.transition = 'all 0.5s ease-in-out';
     slideRef.current.style.transform = `translateX(-${currentSlide * 800}px)`;
   }, [currentSlide]);
+
   return (
     <>
-      <MediaModal
-        isOpen={isOpenMediaModal}
-        closeClick={closeClick}
-        media={modalSrc}
-      />
       <Style.CarouselContainer>
         <Style.SliderContainer ref={slideRef} mediaCount={mediaCount}>
           {mediaList.map((media, index) =>
             /\.(?:jpg|gif|png)/g.test(media) ? (
               <Style.Image
                 src={media}
-                onClick={imageClick}
+                onClick={() => {
+                  showPopup(media);
+                }}
                 key={'media-' + index}
               />
             ) : (
