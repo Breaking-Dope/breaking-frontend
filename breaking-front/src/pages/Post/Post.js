2e305a3600d1e3ddf11e4264512d933e79456652
Yoon-Hae-Min <yunhatmi@naver.com>
2022-07-26 19:49:18 +0900

    style: 게시글 작성 페이지 UI 생성

--- breaking-front/src/pages/Post/Post.js
+++ breaking-front/src/pages/Post/Post.js
@@ -1,34 +1,146 @@
-import React from 'react';
-import Button from 'components/Button/Button';
-import Line from 'components/Line/Line';
+import React, { useState } from 'react';
 import * as Style from 'pages/Post/Post.styles';
 import PostUploadForm from 'pages/Post/units/PostUploadForm';
+import { ReactComponent as LocationIcon } from 'assets/svg/location.svg';
+import 'react-datepicker/dist/react-datepicker.css';
+import dayjs from 'dayjs';
 
 const Post = () => {
+  const [occurDate, setOccurDate] = useState(dayjs().format('YYYY-MM-DD'));
+  const [occurTime, setOccurTime] = useState(dayjs().format('HH:mm'));
+  const [selectedPostType, setSelectedPostType] = useState('charged');
+  const [selectedAnonymous, setSelectedAnonymous] = useState('public');
+  const [price, setPrice] = useState();
+  const [isShowPriceInput, setIsShowPriceInput] = useState(true);
+  const handlePostType = (event) => {
+    setSelectedPostType(event.target.id);
+  };
+  const handleAnonymous = (event) => {
+    setSelectedAnonymous(event.target.id);
+  };
+  const handleOccurDate = (event) => {
+    setOccurDate(event.target.value);
+  };
+  const handleOccurTime = (event) => {
+    setOccurTime(event.target.value);
+  };
+  const handlePrice = (event) => {
+    setPrice(Number(event.target.value));
+  };
+  const toggleShowPirceInput = () => {
+    setIsShowPriceInput((pre) => !pre);
+  };
+  const maxLengthCheck = ({ target }) => {
+    console.log(target.value);
+    if (target.value.length > target.maxLength) {
+      target.value = target.value.slice(0, target.maxLength);
+    }
+  };
   return (
     <Style.Container>
       <Style.BackSpace></Style.BackSpace>
       <PostUploadForm />
       <Style.OccurTime>
-        <Style.OccurTimeTitle></Style.OccurTimeTitle>
-        <Style.OccurTimeForm></Style.OccurTimeForm>
+        <Style.PostTitle>제보 발생 시간</Style.PostTitle>
+        <Style.OccurTimeForm>
+          <Style.DatePicker
+            type="date"
+            value={occurDate}
+            onChange={handleOccurDate}
+          ></Style.DatePicker>
+          <Style.DatePicker
+            type="time"
+            value={occurTime}
+            onChange={handleOccurTime}
+          ></Style.DatePicker>
+        </Style.OccurTimeForm>
       </Style.OccurTime>
+
       <Style.Location>
-        <Style.LocationTitle>
-          <Style.LocationIcon></Style.LocationIcon>
-        </Style.LocationTitle>
-        <Style.LocationForm></Style.LocationForm>
+        <Style.PostTitle>
+          위치
+          <Style.FindLocation>
+            <LocationIcon />
+            <Style.FindLocationMessage>
+              현재 위치 찾기
+            </Style.FindLocationMessage>
+          </Style.FindLocation>
+        </Style.PostTitle>
+        <Style.LocationForm>
+          <Style.LocationInput type="text" placeholder="주소를 입력하세요" />
+        </Style.LocationForm>
       </Style.Location>
-      <Line />
+
       <Style.Context>
-        <Style.ContextTitle></Style.ContextTitle>
-        <Style.ContextBody></Style.ContextBody>
-        <Style.HashTag></Style.HashTag>
+        <Style.TitleInput
+          type="text"
+          placeholder=" 제목을 입력하세요"
+        ></Style.TitleInput>
+        <Style.BodyTextArea
+          placeholder=" 상황을 최대한 상세하게 기록해 주세요&#13; (상황, 시간, 사건 전개과정, 경과상태 등&#13;&#10; 최대 2000자"
+        ></Style.BodyTextArea>
+        <Style.HashTagInput placeholder=" # 해시태그를 입력하세요. (최대 8개)"></Style.HashTagInput>
       </Style.Context>
-      <Style.PostType></Style.PostType>
-      <Style.Price></Style.Price>
-      <Style.Anonymous></Style.Anonymous>
-      <Button />
+
+      <Style.PostType>
+        <Style.PostTitle>제보 방식</Style.PostTitle>
+        <Style.PostRadioButton
+          onClick={handlePostType}
+          id="charged"
+          radioContorl={selectedPostType}
+        >
+          유료제보
+        </Style.PostRadioButton>
+        <Style.PostRadioButton
+          onClick={handlePostType}
+          id="free"
+          radioContorl={selectedPostType}
+        >
+          무료제보
+        </Style.PostRadioButton>
+        <Style.PostRadioButton
+          onClick={handlePostType}
+          id="exclusive"
+          radioContorl={selectedPostType}
+        >
+          단독제보
+        </Style.PostRadioButton>
+      </Style.PostType>
+
+      <Style.Price>
+        <Style.PostTitle>제보 가격</Style.PostTitle>
+        <Style.PostPriceInput
+          type="text"
+          onInput={maxLengthCheck}
+          maxLength="15"
+          value={
+            isShowPriceInput ? price : price.toLocaleString('ko-KR') + '원'
+          }
+          onChange={handlePrice}
+          onBlur={toggleShowPirceInput}
+          onFocus={toggleShowPirceInput}
+        ></Style.PostPriceInput>
+      </Style.Price>
+
+      <Style.Anonymous>
+        <Style.PostTitle>프로필을 공개하시겠습니까?</Style.PostTitle>
+        <Style.PostRadioButton
+          onClick={handleAnonymous}
+          id="private"
+          radioContorl={selectedAnonymous}
+        >
+          비공개
+        </Style.PostRadioButton>
+        <Style.PostRadioButton
+          onClick={handleAnonymous}
+          id="public"
+          radioContorl={selectedAnonymous}
+        >
+          공개
+        </Style.PostRadioButton>
+      </Style.Anonymous>
+
+      <Style.PostSubmitButton type="submit">제보 하기</Style.PostSubmitButton>
     </Style.Container>
   );
 };
--- breaking-front/src/pages/Post/Post.styles.js
+++ breaking-front/src/pages/Post/Post.styles.js
@@ -1,33 +1,167 @@
-import styled from 'styled-components';
+import Button from 'components/Button/Button';
+import styled, { css } from 'styled-components';
+
+export const PostTitle = styled.h3`
+  font-size: 24px;
+`;
+
+export const PostRadioButton = styled(Button)`
+  ${({ id, radioContorl }) =>
+    id === radioContorl &&
+    css`
+      background-color: ${({ theme }) => theme.blue[400]};
+    `}
+`;
+
+export const PostSubmitButton = styled(Button)`
+  display: block;
+  margin: 0 auto 50px;
+  width: 170px;
+  background-color: ${({ theme }) => theme.blue[400]};
+`;
 
 export const Container = styled.div``;
 
 export const BackSpace = styled.div``;
 
-export const OccurTime = styled.div``;
+export const OccurTime = styled.div`
+  margin-top: 60px;
+  > h3 {
+    margin-bottom: 20px;
+  }
+`;
 
-export const OccurTimeTitle = styled.div``;
+export const DatePicker = styled.input`
+  position: relative;
+  padding: 10px;
+  margin-right: 10px;
+  width: 230px;
+  border-radius: 10px;
+  border: solid 1px ${({ theme }) => theme.gray[500]};
+  ::-webkit-calendar-picker-indicator {
+    position: absolute;
+    background: transparent;
+    bottom: 0;
+    color: transparent;
+    cursor: pointer;
+    height: auto;
+    left: 0;
+    right: 0;
+    top: 0;
+    width: auto;
+  }
+`;
 
 export const OccurTimeForm = styled.div``;
 
-export const Location = styled.div``;
+export const Location = styled.div`
+  margin-top: 60px;
+`;
 
-export const LocationTitle = styled.div``;
+export const FindLocation = styled.div`
+  display: inline-block;
+  margin-left: 20px;
+  cursor: pointer;
+  > svg {
+    vertical-align: middle;
+    width: 15px;
+    height: 15px;
+    > path {
+      stroke: ${({ theme }) => theme.blue[900]};
+    }
+  }
+`;
 
-export const LocationIcon = styled.div``;
+export const FindLocationMessage = styled.span`
+  vertical-align: middle;
+  font-size: 16px;
+  color: ${({ theme }) => theme.blue[900]};
+`;
 
-export const LocationForm = styled.div``;
+export const LocationForm = styled.div`
+  margin-top: 20px;
+  margin-bottom: 80px;
+`;
 
-export const Context = styled.div``;
+export const LocationInput = styled.input`
+  padding: 10px;
+  width: 470px;
+  border: solid 1px ${({ theme }) => theme.gray[500]};
+  border-radius: 10px;
+`;
 
-export const ContextTitle = styled.div``;
+export const Context = styled.div`
+  margin-top: 60px;
+  > * {
+    margin-bottom: 40px;
+  }
+`;
 
-export const ContextBody = styled.div``;
+export const TitleInput = styled.input`
+  width: 100%;
+  font-size: 18px;
 
-export const HashTag = styled.div``;
+  border: none;
+  border-left: solid 3px ${({ theme }) => theme.gray[700]};
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    font-weight: 100;
+    color: ${({ theme }) => theme.gray[400]};
+  }
+`;
 
-export const PostType = styled.div``;
+export const BodyTextArea = styled.textarea`
+  padding: 10px;
+  width: 100%;
+  height: 300px;
+  resize: none;
+  border: solid 1px ${({ theme }) => theme.gray[500]};
+  border-radius: 10px;
+  &::placeholder {
+    color: ${({ theme }) => theme.gray[400]};
+  }
+`;
 
-export const Price = styled.div``;
+export const HashTagInput = styled(TitleInput)`
+  font-size: 12px;
+  color: ${({ theme }) => theme.blue[900]};
+`;
 
-export const Anonymous = styled.div``;
+export const PostType = styled.div`
+  margin-top: 40px;
+  > button {
+    margin-right: 30px;
+  }
+  > * {
+    margin-bottom: 30px;
+  }
+`;
+
+export const Price = styled.div`
+  margin-top: 40px;
+  > * {
+    margin-bottom: 30px;
+  }
+`;
+
+export const PostPriceInput = styled.input`
+  padding: 10px;
+  font-size: 16px;
+  border: solid 1px ${({ theme }) => theme.gray[500]};
+  border-radius: 10px;
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+  }
+`;
+
+export const Anonymous = styled.div`
+  margin-top: 40px;
+  > button {
+    margin-right: 30px;
+  }
+  > * {
+    margin-bottom: 30px;
+  }
+`;
