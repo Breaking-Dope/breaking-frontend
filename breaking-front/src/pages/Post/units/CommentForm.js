172f72e265f2c4ea4465462e7b378f4be11973c2
kangju2000 <kangju2000@naver.com>
2022-07-27 14:30:44 +0900

    feat: CommentForm 컴포넌트 기능 추가

--- breaking-front/src/pages/Post/units/CommentForm.js
+++ breaking-front/src/pages/Post/units/CommentForm.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { PAGE_PATH } from 'constants/path';
 import ProfileImage from 'components/ProfileImage/ProfileImage';
 import * as Style from 'pages/Post/units/CommentForm.styles';
+import { useMutation, useQueryClient } from 'react-query';
+import { postPostCommentWrite } from 'api/post';
 
-const CommentForm = ({ profileImgURL }) => {
+const CommentForm = ({ profileImgURL, userId, postId }) => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
   const [comment, setComment] = useState('');
+
+  const { mutate: CommentWrite } = useMutation(postPostCommentWrite, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['postComment']);
+      queryClient.invalidateQueries(['postReply']);
+    },
+  });
+
+  const handleProfileClick = () => {
+    navigate(PAGE_PATH.PROFILE(userId));
+  };
 
   const handleChange = (event) => {
     setComment(event.target.value);
@@ -12,12 +29,18 @@ const CommentForm = ({ profileImgURL }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    comment && CommentWrite({ postId: postId, data: comment });
     setComment('');
   };
   return (
     <>
       <Style.CommentForm onSubmit={handleSubmit}>
-        <ProfileImage size="medium" src={profileImgURL} />
+        <ProfileImage
+          size="medium"
+          src={profileImgURL}
+          profileClick={handleProfileClick}
+        />
         <Style.CommentInput
           placeholder="댓글 추가"
           onChange={handleChange}
@@ -30,6 +53,8 @@ const CommentForm = ({ profileImgURL }) => {
 };
 CommentForm.propTypes = {
   profileImgURL: PropTypes.string,
+  userId: PropTypes.number,
+  postId: PropTypes.number,
 };
 
 export default CommentForm;
