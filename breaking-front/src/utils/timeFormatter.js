dfbd19e4d2189e8a8f040b47b9432b6f538092f6
kangju2000 <kangju2000@naver.com>
2022-08-04 10:22:27 +0900

    refactor: timeFormatter에서 '전' 앞에 한 칸 띄우도록 수정

--- breaking-front/src/utils/timeFormatter.js
+++ breaking-front/src/utils/timeFormatter.js
@@ -4,25 +4,25 @@ const timeFormatter = (date) => {
   let interval = seconds / 31536000;
 
   if (interval > 1) {
-    return Math.floor(interval) + '년전';
+    return Math.floor(interval) + '년 전';
   }
   interval = seconds / 2592000;
   if (interval > 1) {
-    return Math.floor(interval) + '달전';
+    return Math.floor(interval) + '달 전';
   }
   interval = seconds / 86400;
   if (interval >= 1) {
-    return Math.floor(interval) + '일전';
+    return Math.floor(interval) + '일 전';
   }
   interval = seconds / 3600;
   if (interval >= 1) {
-    return Math.floor(interval) + '시간전';
+    return Math.floor(interval) + '시간 전';
   }
   interval = seconds / 60;
   if (interval >= 1) {
-    return Math.floor(interval) + '분전';
+    return Math.floor(interval) + '분 전';
   }
-  return Math.floor(seconds) + '초전';
+  return Math.floor(seconds) + '초 전';
 };
 
 export default timeFormatter;
