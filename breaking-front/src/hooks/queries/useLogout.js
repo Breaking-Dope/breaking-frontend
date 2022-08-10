461881d36435d2da7629ccb597b8ff9b68908e80
kangju2000 <kangju2000@naver.com>
2022-08-10 17:39:35 +0900

    fix: 로그아웃시 Header에 로그인 버튼이 생기지 않는 문제 해결

--- breaking-front/src/hooks/queries/useLogout.js
+++ breaking-front/src/hooks/queries/useLogout.js
@@ -1,19 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+import { useQuery, useQueryClient } from 'react-query';
 import { getLogout } from 'api/login';
 import { PAGE_PATH } from 'constants/path';
-import { useQuery, useQueryClient } from 'react-query';
-import { useNavigate } from 'react-router-dom';
 
 const useLogout = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
-
   return useQuery(['logout'], getLogout, {
     enabled: false,
     onSuccess: () => {
       alert('로그아웃 되었습니다.');
       localStorage.removeItem('access_token');
       //refresh 토큰 또한 제거 예정
-      queryClient.clear();
+      queryClient.invalidateQueries('jwtValidate');
       navigate(PAGE_PATH.LOGIN);
     },
   });
