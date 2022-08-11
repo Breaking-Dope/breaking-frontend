bf191f0c91435def528edf90f4ae173bd27eca45
kangju2000 <kangju2000@naver.com>
2022-08-11 14:24:58 +0900

    refactor: font-feature-settings 스타일을 추가하여 고정폭 숫자를 사용하도록 함

--- breaking-front/src/styles/GlobalStyle.js
+++ breaking-front/src/styles/GlobalStyle.js
@@ -22,10 +22,12 @@ const GlobalStyle = createGlobalStyle`
     src: url(${PretendardBold}) format('woff');
   }
 
-  body{
+  body {
     overflow-y: scroll;
     font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    font-feature-settings: "tnum";
   }
+
   a {
     text-decoration: none;
     color: ${({ theme }) => theme.black};
@@ -34,6 +36,7 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    font-feature-settings: "tnum";
   }
 `;
 
