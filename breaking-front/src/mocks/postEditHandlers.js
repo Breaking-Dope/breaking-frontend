f365d253f97d73c545cefd5447ed7844b6bfed0e
Yoon-Hae-Min <yunhatmi@naver.com>
2022-08-14 20:47:58 +0900

    style: 받는 url을 동적으로 변경

--- breaking-front/src/mocks/postEditHandlers.js
+++ breaking-front/src/mocks/postEditHandlers.js
@@ -2,7 +2,7 @@ import { API_PATH } from 'constants/path';
 import { rest } from 'msw';
 
 export const postEditHandlers = [
-  rest.put(API_PATH.POST_EDIT('*'), (req, res, ctx) => {
+  rest.put(API_PATH.POST_EDIT(':postId'), (req, res, ctx) => {
     console.log(req.body);
     return res(ctx.delay(2000), ctx.json({ postId: 0 }), ctx.status(200));
   }),
