e241ee1366e9a5c4d5c87ac5c8b12ab307bb2fa6
kangju2000 <kangju2000@naver.com>
2022-08-15 19:36:08 +0900

    refactor: 토글의 border 색상을 연한 회색으로 변경

--- breaking-front/src/components/Toggle/Toggle.styles.js
+++ breaking-front/src/components/Toggle/Toggle.styles.js
@@ -4,7 +4,6 @@ export const ToggleContainer = styled.div`
   position: relative;
   display: inline-block;
   z-index: 99;
-  margin-top: 20px;
 `;
 
 export const ArrowMark = styled.div`
@@ -15,7 +14,7 @@ export const ArrowMark = styled.div`
   right: 30px;
   width: 25px;
   height: 25px;
-  border: 1px solid ${({ theme }) => theme.black};
+  border: 1px solid ${({ theme }) => theme.gray[500]};
   border-width: 1px 0px 0px 1px;
   background-color: ${({ theme }) => theme.white};
   transform: rotate(45deg);
@@ -25,7 +24,7 @@ export const Toggle = styled.div`
   position: relative;
   width: ${({ width }) => width};
   padding: 15px 10px 5px;
-  border: 1px solid ${({ theme }) => theme.black};
+  border: 1px solid ${({ theme }) => theme.gray[500]};
   border-radius: 10px;
   background-color: ${({ theme }) => theme.white};
 `;
