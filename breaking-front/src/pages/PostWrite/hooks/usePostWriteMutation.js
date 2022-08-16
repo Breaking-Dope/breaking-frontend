a7bcc14ef79bd971959ce25816a533570115c290
Yoon-Hae-Min <yunhatmi@naver.com>
2022-08-16 15:30:54 +0900

    feat: 작성 완료시 redirect를 세부 게시글로 설정

--- breaking-front/src/pages/PostWrite/hooks/usePostWriteMutation.js
+++ breaking-front/src/pages/PostWrite/hooks/usePostWriteMutation.js
@@ -6,9 +6,9 @@ import { useNavigate } from 'react-router-dom';
 const usePostWriteMutation = () => {
   const navigate = useNavigate();
   return useMutation(postPostWrite, {
-    onSuccess: () => {
+    onSuccess: (res) => {
       alert('작성되었습니다.');
-      navigate(PAGE_PATH.HOME);
+      navigate(PAGE_PATH.POST(res.data.postId));
     },
     onError: (error) => {
       console.log(error);
