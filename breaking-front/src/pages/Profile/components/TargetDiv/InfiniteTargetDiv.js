b2cf5315d1f21a85655d07d949ddb4e18b70e18a
Yoon-Hae-Min <yunhatmi@naver.com>
2022-08-18 23:40:56 +0900

    feat: infiniteTarget 컴포넌트 생성

---
+++ breaking-front/src/pages/Profile/components/TargetDiv/InfiniteTargetDiv.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as Style from 'pages/Profile/components/TargetDiv/InfiniteTargetDiv.styles';
+import { useTheme } from 'styled-components';
+import PropTypes from 'prop-types';
+
+const InfiniteTargetDiv = ({ targetRef, isFetching }) => {
+  const theme = useTheme();
+  return (
+    <Style.TargetDiv ref={targetRef}>
+      {isFetching && (
+        <Style.Loading type="spin" color={theme.blue[900]} width="40px" />
+      )}
+    </Style.TargetDiv>
+  );
+};
+
+InfiniteTargetDiv.propTypes = {
+  targetRef: PropTypes.object,
+  isFetching: PropTypes.bool,
+};
+
+export default InfiniteTargetDiv;
