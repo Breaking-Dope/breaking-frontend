93ee013f6df70bcecfc3ced836fd891b90e4ef55
kangju2000 <kangju2000@naver.com>
2022-08-20 21:37:21 +0900

    refactor: toLocaleString 메서드 추가

--- breaking-front/src/pages/Financial/Financial.js
+++ breaking-front/src/pages/Financial/Financial.js
@@ -14,23 +14,27 @@ const Financial = () => {
   const { mutate: Withdraw } = useWithdraw();
 
   const handleDepositSubmit = ({ amount }) => {
-    let depositConfirm = window.confirm(`${amount}원을 충전하시겠습니까?`);
+    const depositConfirm = window.confirm(
+      `${amount.toLocaleString('ko-KR')}원을 충전하시겠습니까?`
+    );
 
     depositConfirm &&
       Deposit(amount, {
         onSuccess: () => {
-          alert(`${amount}원이 충전되었습니다.`);
+          alert(`${amount.toLocaleString('ko-KR')}원이 충전되었습니다.`);
         },
       });
   };
 
   const handleWithdrawSubmit = ({ amount }) => {
-    let withdrawConfirm = window.confirm(`${amount}원을 출금하시겠습니까?`);
+    const withdrawConfirm = window.confirm(
+      `${amount.toLocaleString('ko-KR')}원을 출금하시겠습니까?`
+    );
 
     withdrawConfirm &&
       Withdraw(amount, {
         onSuccess: () => {
-          alert(`${amount}원이 출금되었습니다.`);
+          alert(`${amount.toLocaleString('ko-KR')}원이 출금되었습니다.`);
         },
       });
   };
