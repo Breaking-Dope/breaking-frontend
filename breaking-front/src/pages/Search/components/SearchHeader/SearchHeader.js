232cd1ac344e5ac189309587046086709fd75eee
Yoon-Hae-Min <yunhatmi@naver.com>
2022-08-21 17:46:09 +0900

    feat: header클릭시 각 페이지로 이동

--- breaking-front/src/pages/Search/components/SearchHeader/SearchHeader.js
+++ breaking-front/src/pages/Search/components/SearchHeader/SearchHeader.js
@@ -1,22 +1,52 @@
 import * as Style from 'pages/Search/components/SearchHeader/SearchHeader.styles';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import { PAGE_PATH } from 'constants/path';
 
 const SearchHeader = ({ focusTab, children }) => {
+  const navigate = useNavigate();
+  const SearchUnifiedClick = () => {
+    navigate(PAGE_PATH.SEARCH('unified'));
+  };
+
+  const SearchPostClick = () => {
+    navigate(PAGE_PATH.SEARCH('post'));
+  };
+
+  const SearchHashtagClick = () => {
+    navigate(PAGE_PATH.SEARCH('hashtag'));
+  };
+
+  const SearchUserClick = () => {
+    navigate(PAGE_PATH.SEARCH('user'));
+  };
   return (
     <>
       <Style.SearchTabs>
         <Style.SearchTabList>
-          <Style.SearchTabItem isActiveTabItem={focusTab === 0}>
+          <Style.SearchTabItem
+            isActiveTabItem={focusTab === 0}
+            onClick={SearchUnifiedClick}
+          >
             통합 검색
           </Style.SearchTabItem>
-          <Style.SearchTabItem isActiveTabItem={focusTab === 1}>
+          <Style.SearchTabItem
+            isActiveTabItem={focusTab === 1}
+            onClick={SearchPostClick}
+          >
             게시글
           </Style.SearchTabItem>
-          <Style.SearchTabItem isActiveTabItem={focusTab === 2}>
+          <Style.SearchTabItem
+            isActiveTabItem={focusTab === 2}
+            onClick={SearchHashtagClick}
+          >
             해시태그
           </Style.SearchTabItem>
-          <Style.SearchTabItem isActiveTabItem={focusTab === 3}>
+          <Style.SearchTabItem
+            isActiveTabItem={focusTab === 3}
+            onClick={SearchUserClick}
+          >
             사람
           </Style.SearchTabItem>
         </Style.SearchTabList>
