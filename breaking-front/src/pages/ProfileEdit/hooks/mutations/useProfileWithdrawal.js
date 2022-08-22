3dc6034ab881d1e3ee49d2b430f83bb79c1e3467
kangju2000 <kangju2000@naver.com>
2022-08-22 10:50:44 +0900

    refactor: ProfileEdit 페이지의 hook 분리

---
+++ breaking-front/src/pages/ProfileEdit/hooks/mutations/useProfileWithdrawal.js
@@ -0,0 +1,17 @@
+import { deleteProfileWithdrawal } from 'api/profile';
+import { PAGE_PATH } from 'constants/path';
+import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+
+const usePostWriteMutation = () => {
+  const navigate = useNavigate();
+
+  return useMutation(deleteProfileWithdrawal, {
+    onSuccess: () => {
+      alert('탈퇴 완료되었습니다.');
+      navigate(PAGE_PATH.HOME);
+    },
+  });
+};
+
+export default usePostWriteMutation;
