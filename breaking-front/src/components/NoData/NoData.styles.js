79c31e559b817326612e1b7a28270ce6f3d2a2e8
KangJH(주기) <kangju2000@naver.com>
2022-08-23 15:43:37 +0900

    feat: NoData 컴포넌트 구현 (#207)
    * feat: NoData 컴포넌트 구현
    
    * refactor: NoData 컴포넌트 스타일 수정

---
+++ breaking-front/src/components/NoData/NoData.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const NoData = styled.div`
+  display: flex;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Message = styled.p``;
