432d1012268784d95b3001b1e066c3b9d2300f6a
kangju2000 <kangju2000@naver.com>
2022-08-23 14:16:26 +0900

    refactor: Reply 컴포넌트 로직 수정

--- breaking-front/src/pages/Post/components/Reply/Reply.js
+++ breaking-front/src/pages/Post/components/Reply/Reply.js
@@ -1,37 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import usePostReply from 'pages/Post/hooks/queries/usePostReply';
 import Comment from 'pages/Post/components/Comment/Comment';
+import InfiniteTargetDiv from 'components/InfiniteTargetDiv/InfiniteTargetDiv';
 import * as Style from 'pages/Post/components/Reply/Reply.styles';
 import { ReactComponent as MoreIcon } from 'assets/svg/more_arrow.svg';
-import InfiniteTargetDiv from 'components/InfiniteTargetDiv/InfiniteTargetDiv';
 
-const Reply = ({ replyData, isFetching, FetchNextReply }) => {
+const Reply = ({ isOpen, commentId }) => {
+  const {
+    data: postReplyData,
+    isFetching: isPostReplyFetching,
+    fetchNextPage: FetchNextPostReply,
+  } = usePostReply(commentId);
+
   return (
-    <Style.Reply>
-      {replyData?.pages.map((page) =>
-        page.result.map((reply) => (
-          <Comment comment={reply} type="reply" key={reply.commentId} />
-        ))
-      )}
-      {replyData?.pages.slice(-1)[0].hasNextReply && !isFetching && (
-        <Style.MoreShowReply
-          onClick={() => {
-            FetchNextReply();
-          }}
-        >
-          <MoreIcon />
-          더보기
-        </Style.MoreShowReply>
-      )}
-      <InfiniteTargetDiv isFetching={isFetching} height="0px" />
-    </Style.Reply>
+    isOpen && (
+      <Style.Reply>
+        {postReplyData?.pages.map((page) =>
+          page.result.map((reply) => (
+            <Comment comment={reply} type="reply" key={reply.commentId} />
+          ))
+        )}
+        {postReplyData?.pages.slice(-1)[0].hasNextReply &&
+          !isPostReplyFetching && (
+            <Style.MoreShowReply
+              onClick={() => {
+                FetchNextPostReply();
+              }}
+            >
+              <MoreIcon />
+              더보기
+            </Style.MoreShowReply>
+          )}
+        <InfiniteTargetDiv isFetching={isPostReplyFetching} height="0px" />
+      </Style.Reply>
+    )
   );
 };
 
 Reply.propTypes = {
-  replyData: PropTypes.object,
-  isFetching: PropTypes.bool,
-  FetchNextReply: PropTypes.func,
+  isOpen: PropTypes.bool,
+  commentId: PropTypes.string,
 };
 
 export default Reply;
