e5cea237db7030d9a0c96174be3d5e43d737c6fe
Yoon-Hae-Min <yunhatmi@naver.com>
2022-08-23 13:29:48 +0900

    refactor: FollowCard에서 mutation hook을 상위 컴포넌트에서 다룰수 있게 변경

--- breaking-front/src/pages/Profile/components/FollowCardList/FollowCardList.js
+++ breaking-front/src/pages/Profile/components/FollowCardList/FollowCardList.js
@@ -6,6 +6,8 @@ import { PAGE_PATH } from 'constants/path';
 import { UserInformationContext } from 'providers/UserInformationProvider';
 import FollowCard from 'components/FollowCard/FollowCard';
 import PropTypes from 'prop-types';
+import { useMutation, useQueryClient } from 'react-query';
+import { deleteUnFollow, postFollow } from 'api/profile';
 
 const FollowCardList = ({
   isLoading,
@@ -16,6 +18,40 @@ const FollowCardList = ({
 }) => {
   const navigate = useNavigate();
   const userData = useContext(UserInformationContext);
+
+  const queryClient = useQueryClient();
+  const ChangeIsFollowField = (pre, userId) => {
+    return pre.map((information) =>
+      information.userId === userId
+        ? {
+            ...information,
+            isFollowing: !information.isFollowing,
+          }
+        : information
+    );
+  };
+  // follower following 리스트의 핋드 값을 변경해주어야함
+
+  const UnFollowMutation = useMutation(deleteUnFollow, {
+    onSuccess: (data, userId) => {
+      queryClient.invalidateQueries('profile');
+      setFollowerList((pre) => ChangeIsFollowField(pre, userId));
+      setFollowingList((pre) => ChangeIsFollowField(pre, userId));
+    },
+    onError: () => {
+      //에러처리
+    },
+  });
+  const FollowMutation = useMutation(postFollow, {
+    onSuccess: (data, userId) => {
+      queryClient.invalidateQueries('profile');
+      setFollowerList((pre) => ChangeIsFollowField(pre, userId));
+      setFollowingList((pre) => ChangeIsFollowField(pre, userId));
+    },
+    onError: () => {
+      //에러처리
+    },
+  });
 
   return (
     <>
@@ -29,8 +65,8 @@ const FollowCardList = ({
             isPermission={item.userId !== userData.userId}
             profileData={item}
             key={`follow-${item.userId}`}
-            setFollowingList={setFollowingList}
-            setFollowerList={setFollowerList}
+            FollowMutation={FollowMutation}
+            UnFollowMutation={UnFollowMutation}
           />
         ))}
       {isLoading && (
