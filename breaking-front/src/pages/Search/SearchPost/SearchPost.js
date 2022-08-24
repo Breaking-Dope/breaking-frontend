b635f87da715c96b486962008d6f192f0f9541d2
Yoon-Hae-Min <yunhatmi@naver.com>
2022-08-24 13:52:21 +0900

    feat: 게시글 검색 구현

--- breaking-front/src/pages/Search/SearchPost/SearchPost.js
+++ breaking-front/src/pages/Search/SearchPost/SearchPost.js
@@ -3,29 +3,52 @@ import * as Style from 'pages/Search/SearchPost/SearchPost.styles';
 import SearchHeader from 'pages/Search/components/SearchHeader/SearchHeader';
 import Feed from 'components/Feed/Feed';
 import { UserInformationContext } from 'providers/UserInformationProvider';
-import {
-  EMPTY_PICTURE_CONTENT,
-  EXCLUSIVE_CONTENT,
-  NORMAL_CONTENT,
-  SOLDOUT_CONTENT,
-} from 'mocks/dummyData/contents';
+import useSearch from '../hooks/queries/useSearch';
+import { useSearchParams } from 'react-router-dom';
+import { FeedSkeleton } from 'components/Skeleton/Skeleton';
+import InfiniteTargetDiv from 'components/InfiniteTargetDiv/InfiniteTargetDiv';
+import useInfiniteScroll from 'hooks/useInfiniteScroll';
 
 const SearchPost = () => {
-  const FeedList = [
-    NORMAL_CONTENT,
-    EXCLUSIVE_CONTENT,
-    EMPTY_PICTURE_CONTENT,
-    SOLDOUT_CONTENT,
-  ];
   const { userId } = useContext(UserInformationContext);
+  const [searchParams] = useSearchParams();
+
+  const {
+    data: searchPostResult,
+    isLoading: searchPostLoading,
+    fetchNextPage: FetchNextSearchPost,
+    isFetching: isFetchSearchPost,
+  } = useSearch(searchParams, 10);
+
+  const { targetRef } = useInfiniteScroll(
+    searchPostResult,
+    FetchNextSearchPost
+  );
+
   return (
     <>
       <SearchHeader focusTab={1} />
       <Style.PostResultList>
-        {FeedList.map((feed) => (
-          <Feed feedData={feed} key={feed.postId} userId={userId} />
-        ))}
+        {searchPostLoading ? (
+          <>
+            <FeedSkeleton />
+            <FeedSkeleton />
+            <FeedSkeleton />
+            <FeedSkeleton />
+          </>
+        ) : (
+          searchPostResult.pages.map((page) =>
+            page.result.map((feed) => (
+              <Feed feedData={feed} key={feed.postId} userId={userId} />
+            ))
+          )
+        )}
       </Style.PostResultList>
+      <InfiniteTargetDiv
+        targetRef={targetRef}
+        isFetching={isFetchSearchPost}
+        height="80px"
+      />
     </>
   );
 };
