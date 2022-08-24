0c4e0e0a8e4b5e0a30b775be554168fafbe4853a
Yoon-Hae-Min <yunhatmi@naver.com>
2022-08-24 13:59:00 +0900

    feat: 해시태그 검색 구현

--- breaking-front/src/pages/Search/SearchHashtag/SearchHashtag.js
+++ breaking-front/src/pages/Search/SearchHashtag/SearchHashtag.js
@@ -3,29 +3,50 @@ import * as Style from 'pages/Search/SearchHashtag/SearchHashtag.styles';
 import SearchHeader from 'pages/Search/components/SearchHeader/SearchHeader';
 import Feed from 'components/Feed/Feed';
 import { UserInformationContext } from 'providers/UserInformationProvider';
-import {
-  EMPTY_PICTURE_CONTENT,
-  EXCLUSIVE_CONTENT,
-  NORMAL_CONTENT,
-  SOLDOUT_CONTENT,
-} from 'mocks/dummyData/contents';
+import { useSearchParams } from 'react-router-dom';
+import useSearchHashtag from 'pages/Search/hooks/queries/useSearchHashtag';
+import useInfiniteScroll from 'hooks/useInfiniteScroll';
+import InfiniteTargetDiv from 'components/InfiniteTargetDiv/InfiniteTargetDiv';
+import { FeedSkeleton } from 'components/Skeleton/Skeleton';
 
 const SearchHashtag = () => {
-  const FeedList = [
-    NORMAL_CONTENT,
-    EXCLUSIVE_CONTENT,
-    EMPTY_PICTURE_CONTENT,
-    SOLDOUT_CONTENT,
-  ];
   const { userId } = useContext(UserInformationContext);
+  const [searchParams] = useSearchParams();
+
+  const {
+    data: searchHashtagResult,
+    isLoading: searchHashtagLoading,
+    fetchNextPage: FetchNextSearchHashtag,
+    isFetching: isFetchSearchHashtag,
+  } = useSearchHashtag(searchParams, 10);
+
+  const { targetRef } = useInfiniteScroll(
+    searchHashtagResult,
+    FetchNextSearchHashtag
+  );
   return (
     <>
       <SearchHeader focusTab={2} />
       <Style.PostResultList>
-        {FeedList.map((feed) => (
-          <Feed feedData={feed} key={feed.postId} userId={userId} />
-        ))}
+        {searchHashtagLoading ? (
+          <>
+            <FeedSkeleton />
+            <FeedSkeleton />
+            <FeedSkeleton />
+            <FeedSkeleton />
+          </>
+        ) : (
+          searchHashtagResult.pages.map((page) =>
+            page.result.map((feed) => (
+              <Feed feedData={feed} key={feed.postId} userId={userId} />
+            ))
+          )
+        )}
       </Style.PostResultList>
+      <InfiniteTargetDiv
+        targetRef={targetRef}
+        isFetching={isFetchSearchHashtag}
+      />
     </>
   );
 };
