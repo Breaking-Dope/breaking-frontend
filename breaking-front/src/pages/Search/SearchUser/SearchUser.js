d0c0c7f8c010b71a7f548e35b2a99e575b21ffdc
Yoon-Hae-Min <yunhatmi@naver.com>
2022-08-24 14:30:25 +0900

    feat: 유저 검색 구현

--- breaking-front/src/pages/Search/SearchUser/SearchUser.js
+++ breaking-front/src/pages/Search/SearchUser/SearchUser.js
@@ -1,31 +1,50 @@
 import React from 'react';
 import SearchHeader from 'pages/Search/components/SearchHeader/SearchHeader';
-import SearchUserResultCard from 'pages/Search/SearchUser/components/SearchUserResultCard';
+import SearchUserResultCard from 'pages/Search/SearchUser/components/SearchUserResultCard/SearchUserResultCard';
 import * as Style from 'pages/Search/SearchUser//SearchUser.styles';
-import {
-  FOLLOWING_USER,
-  NORMAL_USER,
-  NO_FOLLOW_USER,
-  NO_POSTCOUNT_USER,
-  NO_PROFILEIMGURL_USER,
-} from 'mocks/dummyData/users';
+import SearchUserResultCardSkeleton from 'pages/Search/SearchUser/components/SearchUserResultCardSkeleton/SearchUserResultCardSkeleton';
+import { useSearchParams } from 'react-router-dom';
+import useSearchUser from 'pages/Search/hooks/queries/useSearchUser';
+import InfiniteTargetDiv from 'components/InfiniteTargetDiv/InfiniteTargetDiv';
+import useInfiniteScroll from 'hooks/useInfiniteScroll';
 
 const SearchUser = () => {
-  const userList = [
-    NO_PROFILEIMGURL_USER,
-    NORMAL_USER,
-    NO_POSTCOUNT_USER,
-    NO_FOLLOW_USER,
-    FOLLOWING_USER,
-  ];
+  const [searchParams] = useSearchParams();
+  const {
+    data: searchUserResult,
+    isLoading: isSearchUserLoading,
+    isFetching: isSearchUserFetching,
+    fetchNextPage: FetchNextSearchUser,
+  } = useSearchUser(searchParams.get('query'), 10);
+  const { targetRef } = useInfiniteScroll(
+    searchUserResult,
+    FetchNextSearchUser
+  );
   return (
     <>
       <SearchHeader focusTab={3} />
       <Style.SearchUserLayout>
-        {userList.map((user) => (
-          <SearchUserResultCard user={user} key={user.userId} />
-        ))}
+        {isSearchUserLoading ? (
+          <>
+            <SearchUserResultCardSkeleton />
+            <SearchUserResultCardSkeleton />
+            <SearchUserResultCardSkeleton />
+            <SearchUserResultCardSkeleton />
+            <SearchUserResultCardSkeleton />
+            <SearchUserResultCardSkeleton />
+          </>
+        ) : (
+          searchUserResult.pages.map((page) =>
+            page.result.map((user) => (
+              <SearchUserResultCard user={user} key={user.userId} />
+            ))
+          )
+        )}
       </Style.SearchUserLayout>
+      <InfiniteTargetDiv
+        targetRef={targetRef}
+        isFetching={isSearchUserFetching}
+      />
     </>
   );
 };
