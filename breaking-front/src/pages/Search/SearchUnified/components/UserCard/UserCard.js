b457f584eb80842c3b9b108de1e2cec54b63dc2f
Yoon-Hae-Min <yunhatmi@naver.com>
2022-08-24 12:51:38 +0900

    feat: 카드 클릭시에 해당 프로필 페이지로 이동

--- breaking-front/src/pages/Search/SearchUnified/components/UserCard/UserCard.js
+++ breaking-front/src/pages/Search/SearchUnified/components/UserCard/UserCard.js
@@ -3,12 +3,22 @@ import PropTypes from 'prop-types';
 import ProfileImage from 'components/ProfileImage/ProfileImage';
 import * as Style from 'pages/Search/SearchUnified/components/UserCard/UserCard.styles';
 import Button from 'components/Button/Button';
+import { useNavigate } from 'react-router-dom';
+import { PAGE_PATH } from 'constants/path';
 
 const UserCard = ({ user }) => {
+  const navigate = useNavigate();
+  const CardClick = () => {
+    navigate(PAGE_PATH.PROFILE(user.userId));
+  };
   return (
     <Style.UserInformationContainer>
-      <ProfileImage size="medium" src={user.profileImgURL} />
-      <Style.UserName>{user.nickname}</Style.UserName>
+      <ProfileImage
+        size="medium"
+        src={user.profileImgURL}
+        onClick={CardClick}
+      />
+      <Style.UserName onClick={CardClick}>{user.nickname}</Style.UserName>
       <Style.UserStatusMsg>{user.statusMsg}</Style.UserStatusMsg>
       {user.isFollowing ? (
         <Button size="small">언팔로우</Button>
