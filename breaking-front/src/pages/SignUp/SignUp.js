889765447a82b0d2405e64ab50b031ccf3545d2a
kangju2000 <kangju2000@naver.com>
2022-08-27 00:56:47 +0900

    refactor: SignUp 페이지 로직 일부 수정

--- breaking-front/src/pages/SignUp/SignUp.js
+++ breaking-front/src/pages/SignUp/SignUp.js
@@ -1,32 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { PAGE_PATH } from 'constants/path';
 import ProfileSettingForm from 'components/ProfileSettingForm/ProfileSettingForm';
 import MESSAGE from 'constants/message';
 import useSignUp from 'pages/SignUp/hooks/mutations/useSignUp';
+import { UserInformationContext } from 'providers/UserInformationProvider';
 
 const SignUp = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const { isLogin } = useContext(UserInformationContext);
 
   const { mutate: SignUpMutate, isLoading: isSignUpLoading } = useSignUp();
 
   useEffect(() => {
-    //유저가 sns 로그인하지않고 회원가입 페이지로 들어왔을 때 처리
     if (!location.state) {
-      alert(MESSAGE.SIGNUP.WRONG_ACCESS);
-      navigate(PAGE_PATH.LOGIN);
+      if (isLogin) navigate(PAGE_PATH.HOME);
+      else {
+        alert(MESSAGE.SIGNUP.WRONG_ACCESS);
+        navigate(PAGE_PATH.LOGIN);
+      }
     }
   }, []);
 
   return (
     <>
-      <ProfileSettingForm
-        pageType="signUp"
-        username={location.state?.username}
-        isProfileMutateLoading={isSignUpLoading}
-        mutate={SignUpMutate}
-      />
+      {location.state && (
+        <ProfileSettingForm
+          pageType="signUp"
+          username={location.state.username}
+          isProfileMutateLoading={isSignUpLoading}
+          mutate={SignUpMutate}
+        />
+      )}
     </>
   );
 };
