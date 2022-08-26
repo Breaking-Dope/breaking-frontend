8c628013443ba6ed24c4e5acc36b7e082173872e
kangju2000 <kangju2000@naver.com>
2022-08-26 12:21:11 +0900

    refactor: 오타 수정

--- breaking-front/src/__test__/utils/LikeCount.test.js
+++ breaking-front/src/__test__/utils/LikeCount.test.js
@@ -7,7 +7,7 @@ describe('LikeCount 테스트', () => {
   test('좋아요 개수가 0일 때', () => {
     expect(LikeCount(undefined, 0)).toEqual('좋아요를 눌러보세요.');
   });
-  test('닉네임이 undefined고 좋아요 개수가 존재할 떄', () => {
+  test('닉네임이 undefined고 좋아요 개수가 존재할 때', () => {
     expect(LikeCount(undefined, 1)).toEqual('1');
   });
   test('닉네임이 존재하고 좋아요 개수가 1일 때', () => {
