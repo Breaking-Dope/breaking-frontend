7e23227cccd943bf929afa1855676a1d3b28c064
kangju2000 <kangju2000@naver.com>
2022-08-26 11:28:18 +0900

    refactor: useFollow, useUnFollow option props 추가

--- breaking-front/src/hooks/mutations/useUnFollow.js
+++ breaking-front/src/hooks/mutations/useUnFollow.js
@@ -1,15 +1,8 @@
 import { deleteUnFollow } from 'api/profile';
 import { useMutation } from 'react-query';
 
-const useUnFollow = ({ onSuccess }) => {
-  return useMutation(deleteUnFollow, {
-    onSuccess: () => {
-      onSuccess();
-    },
-    onError: () => {
-      //에러처리
-    },
-  });
+const useUnFollow = (option) => {
+  return useMutation(deleteUnFollow, option);
 };
 
 export default useUnFollow;
