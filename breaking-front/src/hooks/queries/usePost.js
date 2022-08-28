77ace6aea9177ce6efaa37bc10c6d51997774caa
kangju2000 <kangju2000@naver.com>
2022-08-28 15:52:22 +0900

    refactor: 프로필 페이지와 세부 조회페이지에서 잘못된 id로 접속했을 때 에러 페이지로 이동

--- breaking-front/src/hooks/queries/usePost.js
+++ breaking-front/src/hooks/queries/usePost.js
@@ -1,8 +1,16 @@
 import { getPostData } from 'api/post';
+import { PAGE_PATH } from 'constants/path';
 import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 
 const usePost = (postId) => {
-  return useQuery(['post', postId], getPostData);
+  const navigate = useNavigate();
+
+  return useQuery(['post', postId], getPostData, {
+    onError: (error) => {
+      navigate(PAGE_PATH.ERROR);
+    },
+  });
 };
 
 export default usePost;
