a7e697717d39583be8334a768d560c8dd501b645
Yoon-Hae-Min <yunhatmi@naver.com>
2022-09-05 15:38:51 +0900

    refactor: 대소문자 변경

--- breaking-front/src/utils/extractHashtag.js
+++
@@ -1,4 +0,0 @@
-const ExtractHashtag = (content) =>
-  content.match(/#[^\s#]+/g)?.map((hashtag) => hashtag.replace('#', ''));
-
-export default ExtractHashtag;
--- breaking-front/src/utils/parseAddressName.js
+++
@@ -1,9 +0,0 @@
-const ParseAddressName = (address) => {
-  const splitted = address.split(' ');
-  return {
-    region_1depth_name: splitted[0],
-    region_2depth_name: splitted[1],
-  };
-};
-
-export default ParseAddressName;
