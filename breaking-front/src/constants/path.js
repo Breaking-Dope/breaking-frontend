a1bb1f60a2166dd3318d35f34d2c2291a4094ac2
Yoon-Hae-Min <yunhatmi@naver.com>
2022-09-05 17:31:36 +0900

    feat: breaking mission 경로추가

--- breaking-front/src/constants/path.js
+++ breaking-front/src/constants/path.js
@@ -14,6 +14,7 @@ export const PAGE_PATH = {
   SEARCH_POST: `/search/post`,
   SEARCH_HASHTAG: `/search/hashtag`,
   SEARCH_USER: `/search/user`,
+  BREAKING_MISSION_MAP: 'breaking_mission/map',
   ERROR: `/not_found`,
 };
 
