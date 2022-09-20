d600ae46d916e1c16c284a5e24669cef608f1988
kangju2000 <kangju2000@naver.com>
2022-09-20 17:16:26 +0900

    refactor: 미션 세부 페이지 관련 api 추가 및 mocking 설정

--- breaking-front/src/api/breakingMission.js
+++ breaking-front/src/api/breakingMission.js
@@ -1,10 +1,29 @@
 import { API_PATH } from 'constants/path';
 import api from 'api/api';
 
+export const getMissionPost = ({ queryKey }) => {
+  const [, missionId] = queryKey;
+  return api({
+    method: 'get',
+    url: API_PATH.BREAKING_MISSION_POST(missionId),
+  });
+};
+
 export const getMissionFeeds = async ({ pageParam = 0 }) => {
   const { data } = await api({
     method: 'get',
     url: API_PATH.BREAKING_MISSION_FEEDS(pageParam),
+  });
+  return {
+    result: data,
+    cursor: data[data.length - 1]?.missionId,
+  };
+};
+
+export const getMissionPostFeeds = async ({ pageParam = 0 }) => {
+  const { data } = await api({
+    method: 'get',
+    url: API_PATH.BREAKING_MISSION_RELATION_FEEDS(pageParam),
   });
   return {
     result: data,
--- breaking-front/src/mocks/breakingMissionHandlers.js
+++ breaking-front/src/mocks/breakingMissionHandlers.js
@@ -2,9 +2,16 @@ import { API_PATH } from 'constants/path';
 import { rest } from 'msw';
 import {
   CLOSED_MISSION_FEED,
+  MISSION_POST,
   NOT_OPEN_MISSION_FEED,
   OPEN_MISSION_FEED,
 } from 'mocks/dummyData/mission';
+import {
+  EMPTY_PICTURE_CONTENT,
+  EXCLUSIVE_CONTENT,
+  NORMAL_CONTENT,
+  SOLDOUT_CONTENT,
+} from 'mocks/dummyData/contents';
 
 export const breakingMissionHandlers = [
   rest.get(API_PATH.BREAKING_MISSION_FEEDS('*', '*'), (req, res, ctx) => {
@@ -13,5 +20,25 @@ export const breakingMissionHandlers = [
       ctx.delay(1000),
       ctx.json([NOT_OPEN_MISSION_FEED, OPEN_MISSION_FEED, CLOSED_MISSION_FEED])
     );
+  }),
+
+  rest.get(
+    API_PATH.BREAKING_MISSION_RELATION_FEEDS('*', '*'),
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.delay(1000),
+        ctx.json([
+          NORMAL_CONTENT,
+          EXCLUSIVE_CONTENT,
+          EMPTY_PICTURE_CONTENT,
+          SOLDOUT_CONTENT,
+        ])
+      );
+    }
+  ),
+
+  rest.get(API_PATH.BREAKING_MISSION_POST(':missionId'), (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(MISSION_POST));
   }),
 ];
