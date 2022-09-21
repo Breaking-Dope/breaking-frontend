01e747744a360f1f679643389019a78f645adb14
kangju2000 <kangju2000@naver.com>
2022-09-21 11:30:57 +0900

    feat: 미션 관련 피드 인피니티 스크롤링 구현

--- breaking-front/src/pages/BreakingMission/MissionPost/components/MissionPostFeed/MissionPostFeed.js
+++ breaking-front/src/pages/BreakingMission/MissionPost/components/MissionPostFeed/MissionPostFeed.js
@@ -1,19 +1,50 @@
 import React from 'react';
 import Feed from 'components/Feed/Feed';
-import { NORMAL_CONTENT } from 'mocks/dummyData/contents';
 import * as Style from 'pages/BreakingMission/MissionPost/components/MissionPostFeed/MissionPostFeed.styles';
+import useMissionPostFeed from '../../hooks/queries/useMissionPostFeed';
+import useInfiniteScroll from 'hooks/useInfiniteScroll';
+import InfiniteTargetDiv from 'components/InfiniteTargetDiv/InfiniteTargetDiv';
+import { FeedSkeleton } from 'components/Skeleton/Skeleton';
+import PropTypes from 'prop-types';
 
-const MissionPostFeed = () => {
+const MissionPostFeed = ({ missionId }) => {
+  const {
+    data: MissionPostFeed,
+    isLoading: isMissionPostFeedLoading,
+    isFetching: isMissionPostFeedFetching,
+    fetchNextPage: FetchNextMissionPostFeed,
+  } = useMissionPostFeed(missionId);
+
+  const { targetRef } = useInfiniteScroll(
+    MissionPostFeed,
+    FetchNextMissionPostFeed
+  );
+
   return (
     <Style.FeedContainer>
       <Style.MissionPostFeed>
-        <Feed feedData={NORMAL_CONTENT} />
-        <Feed feedData={NORMAL_CONTENT} />
-        <Feed feedData={NORMAL_CONTENT} />
-        <Feed feedData={NORMAL_CONTENT} />
+        {MissionPostFeed?.pages.map((page) =>
+          page.result.map((feed) => <Feed feedData={feed} key={feed.postId} />)
+        )}
+        {isMissionPostFeedLoading && (
+          <>
+            <FeedSkeleton />
+            <FeedSkeleton />
+            <FeedSkeleton />
+            <FeedSkeleton />
+          </>
+        )}
       </Style.MissionPostFeed>
+      <InfiniteTargetDiv
+        targetRef={targetRef}
+        isFetching={isMissionPostFeedFetching}
+      />
     </Style.FeedContainer>
   );
+};
+
+MissionPostFeed.propTypes = {
+  missionId: PropTypes.number.isRequired,
 };
 
 export default MissionPostFeed;
